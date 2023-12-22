601fc7712a22f13d34a43eac4dcc6214d9120934
c.mirkin <ciromirkin@gmail.com>
2023-12-22 11:04:57 -0300

    creacion del componente Icon

---
+++ src/components/Icon.tsx
@@ -0,0 +1,13 @@
+type iconType = 'three-dots' | 'trash-fill' | 'caret-right-fill' | 'caret-left-fill'
+
+interface IconProps {
+  name: iconType
+}
+
+function Icon({ name }: IconProps) {
+  return (
+    <i className={`bi bi-${name}`}></i>
+  )
+}
+
+export default Icon
