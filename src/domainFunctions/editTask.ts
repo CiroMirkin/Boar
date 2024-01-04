3059accc14e042ddcb9d87ad16fb9eb14fec83ce
c.mirkin <ciromirkin@gmail.com>
2024-01-04 10:10:41 -0300

    prueva de la funcion editThisTask en verde

--- src/domainFunctions/editTask.ts
+++ src/domainFunctions/editTask.ts
@@ -1,4 +1,5 @@
 import { columnModel } from "../models/column";
+import { taskModel } from "../models/task";
 
 interface editThisTaskParams {
     taskId: string,
@@ -7,5 +8,19 @@ interface editThisTaskParams {
 }
 
 export const editThisTask = ({ taskId, newTaskText, columns }: editThisTaskParams): columnModel[] => {
-    return columns
+    const newColumns = [...columns].map(column => {
+        const newColumn = column
+        newColumn.taskList = newColumn.taskList.map(task => {
+            if(taskId === task.id) {
+                const createNewTask = (taskId: string): taskModel => ({
+                    id: taskId,
+                    descriptionText: !!newTaskText ? newTaskText : task.descriptionText
+                })
+                return createNewTask(taskId)
+            }
+            return task
+        })
+        return newColumn
+    })
+    return newColumns
 }
