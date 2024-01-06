c29f06f23fbe70edd8abef69ad93f7b6e78142cb
c.mirkin <ciromirkin@gmail.com>
2024-01-06 11:26:16 -0300

    eliminacion del componente Icon el cual fue remplazado por ser poco sostenible

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,17 +1,3 @@
-export type iconType = 'three-dots' | 'trash-fill' | 'caret-right-fill' | 'caret-left-fill' | 'pencil-square' | 'clipboard2-fill'
-
-interface IconProps {
-  name: iconType
-}
-
-function Icon({ name }: IconProps) {
-  return (
-    <i className={`bi bi-${name}`}></i>
-  )
-}
-
-export default Icon
-
 export const ClipboardIcon = ({}) => (<i className="bi bi-clipboard2-fill"></i>)
 export const ThreeDotsIcon = ({}) => (<i className="bi bi-three-dots"></i>)
 export const TrashIcon = ({}) => (<i className="bi bi-trash-fill"></i>)
