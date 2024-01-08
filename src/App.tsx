1ac4279b3e54cb155a432ad4a349c7fb2e16cd1f
c.mirkin <ciromirkin@gmail.com>
2024-01-08 12:50:15 -0300

    extraccion de los pasos del tutorial de inicio en un archivo llamado tutorialSteps.ts

--- src/App.tsx
+++ src/App.tsx
@@ -3,54 +3,7 @@ import { useState } from 'react'
 import Board from './components/Board'
 import { Toaster } from 'react-hot-toast'
 import Joyride from "react-joyride";
-
-const steps = [
-  {
-    content: 'Una aplicación para gestionar tareas.',
-    disableBeacon: true,
-    placement: 'center',
-    target: 'body',
-    title: 'Bienvenido a Boar',
-  },
-  {
-    content: 'Así son las tareas.',
-    disableBeacon: true,
-    target: ".task",
-    showSkipButton: false,
-    title: 'Boar',
-  },
-  {
-    content: 'Con este botón puedes ver las opciones de una tarea.',
-    disableBeacon: true,
-    target: ".task-options",
-    showSkipButton: false,
-    title: 'Boar',
-  },
-  {
-    content: 'Aquí puedes escribir y crear tus tareas.',
-    disableBeacon: true,
-    target: ".column__footer",
-    showSkipButton: false,
-    title: 'Boar',
-  },
-  {
-    content: 'Las columnas representan el estado de las tareas.',
-    disableBeacon: true,
-    target: ".column--first-column",
-    showSkipButton: false,
-    title: 'Boar',
-  },
-  {
-    content: 'Al lado del nombre de una columna están las opciones editar y eliminar.',
-    disableBeacon: true,
-    target: ".column-header",
-    showSkipButton: false,
-    title: 'Boar',
-    locale: {
-      last: 'Terminar tutorial', 
-    },
-  },
-];
+import { tutorialSteps } from './tutorialSteps';
 
 function App() {
   const [ columns, setColumns ] = useState([
@@ -80,7 +33,7 @@ function App() {
   return (
     <>
       <Joyride 
-        steps={steps} 
+        steps={tutorialSteps} 
         showProgress 
         showSkipButton 
         hideCloseButton
---
+++ src/tutorialSteps.ts
@@ -0,0 +1,47 @@
+export const tutorialSteps = [
+    {
+      content: 'Una aplicación para gestionar tareas.',
+      disableBeacon: true,
+      placement: 'center',
+      target: 'body',
+      title: 'Bienvenido a Boar',
+    },
+    {
+      content: 'Así son las tareas.',
+      disableBeacon: true,
+      target: ".task",
+      showSkipButton: false,
+      title: 'Boar',
+    },
+    {
+      content: 'Con este botón puedes ver las opciones de una tarea.',
+      disableBeacon: true,
+      target: ".task-options",
+      showSkipButton: false,
+      title: 'Boar',
+    },
+    {
+      content: 'Aquí puedes escribir y crear tus tareas.',
+      disableBeacon: true,
+      target: ".column__footer",
+      showSkipButton: false,
+      title: 'Boar',
+    },
+    {
+      content: 'Las columnas representan el estado de las tareas.',
+      disableBeacon: true,
+      target: ".column--first-column",
+      showSkipButton: false,
+      title: 'Boar',
+    },
+    {
+      content: 'Al lado del nombre de una columna están las opciones editar y eliminar.',
+      disableBeacon: true,
+      target: ".column-header",
+      showSkipButton: false,
+      title: 'Boar',
+      locale: {
+        last: 'Terminar tutorial', 
+      },
+    },
+  ];
