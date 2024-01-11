6692fbd6ef13be5363fa388e714d1724d9c6f1f4
c.mirkin <ciromirkin@gmail.com>
2024-01-11 12:15:29 -0300

    delete edit task code from taskOptions component

--- src/components/TaskOptions.tsx
+++ src/components/TaskOptions.tsx
@@ -1,6 +1,5 @@
-import { useState } from 'react'
 import './TaskOptions.css'
-import { ClipboardIcon, PencilSquareIcon, TrashIcon } from './Icon'
+import { ClipboardIcon, TrashIcon } from './Icon'
 import toast from 'react-hot-toast'
 import { BTN_COLORS, Btn } from './Btn'
 
@@ -8,8 +7,7 @@ interface TaskOptionsProps {
     taskId: string,
     taskDescription: string
     deleteTask: Function,
-    moveTask: Function,
-    editTask?: Function
+    moveTask: Function
 }
 
 interface options {
@@ -19,9 +17,7 @@ interface options {
     icon?: Function
 }
 
-function TaskOptions({ taskId, deleteTask, moveTask, editTask, taskDescription }: TaskOptionsProps) {
-    const [ doesTheUserEditTheTask, setDoesTheUserEditTheTask ] = useState(false)
-    const [ taskText, setTaskText ] = useState(taskDescription)
+function TaskOptions({ taskId, deleteTask, moveTask, taskDescription }: TaskOptionsProps) {
     const options: options[] = [
         {
             name: "Retroceder",
@@ -47,61 +43,18 @@ function TaskOptions({ taskId, deleteTask, moveTask, editTask, taskDescription }
         },
     ]
 
-    const editTaskPositionInOptionsList = 2;
-    const SpliceParamToInsertAnElementWithoutDeletingElements = 0
-    editTask && options.splice(
-        editTaskPositionInOptionsList, SpliceParamToInsertAnElementWithoutDeletingElements, 
-        {
-            name: 'Editar', 
-            function: () => showEditTaskInput(),
-            colorClassName: BTN_COLORS.PRIMARY,
-            icon: () => <PencilSquareIcon />
-        }
-    )
-    const showEditTaskInput = () => {
-        setDoesTheUserEditTheTask(!doesTheUserEditTheTask)
-    }
-    
-    const editTaskBtnHandleClick = () => {
-        if(doesTheUserEditTheTask && !taskText) {
-            toast.error('La tarea esta vacía')
-        }
-        else if(doesTheUserEditTheTask && editTask) {
-            editTask(taskId, taskText)
-            setDoesTheUserEditTheTask(!doesTheUserEditTheTask)
-        }
-        else setDoesTheUserEditTheTask(!doesTheUserEditTheTask)
-    }
-    
     return (
         <>
-            <li className='task-options__options-first-option'>
-                    {
-                        doesTheUserEditTheTask && 
-                        <div style={{ width: '100%', display: 'flex', justifyContent: 'center', gap: '2px' ,padding: '0.2rem' }}>
-                            <input 
-                                type="text" 
-                                name="Editar descripción de la tarea" 
-                                value={taskText} 
-                                onKeyUp={(e) => e.key == 'Enter' && editTaskBtnHandleClick()}
-                                onChange={(e) => setTaskText(e.target.value)}
-                            />
-                            <Btn onClickHandler={editTaskBtnHandleClick} color={BTN_COLORS.PRIMARY}>
-                                <PencilSquareIcon />
-                            </Btn>
-                        </div>
-                    }
-                </li>
-                {
-                    options.map(option => 
-                        <li key={option.name}>
-                            <Btn onClickHandler={() => option.function()} color={option.colorClassName}>
-                                { option.icon! && option.icon() }
-                                <span>{option.name}</span>
-                            </Btn>
-                        </li>
-                    )
-                }
+            {
+                options.map(option => 
+                    <li key={option.name}>
+                        <Btn onClickHandler={() => option.function()} color={option.colorClassName}>
+                            { option.icon! && option.icon() }
+                            <span>{option.name}</span>
+                        </Btn>
+                    </li>
+                )
+            }
         </>
     )
 }
