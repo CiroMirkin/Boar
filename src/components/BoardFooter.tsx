c71032c4e53ed647a95f37377be56a6e41b4ea8c
c.mirkin <ciromirkin@gmail.com>
2024-02-06 18:47:39 -0300

    eliminacion: toast al crear una columna

--- src/components/BoardFooter.tsx
+++ src/components/BoardFooter.tsx
@@ -1,6 +1,5 @@
 import { useDispatch } from "react-redux"
 import { addColumn } from "../redux/columnsSlice"
-import toast from "react-hot-toast"
 import { Btn } from "./atomic/Btn"
 import { COLORS_CLASS_NAME } from "./atomic/colors"
 
@@ -8,7 +7,6 @@ export function BoardFooter() {
     const dispatch = useDispatch()
     const handleClick = () => {
         dispatch(addColumn('Nueva columna'))
-        toast.success('Columna creada')
     }
 
     return (
