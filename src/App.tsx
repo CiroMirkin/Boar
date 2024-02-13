ada33a6a9e98cce67920c65846332df80c86fe1b
c.mirkin <ciromirkin@gmail.com>
2024-02-13 19:24:09 -0300

    delete components

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,8 @@
 import './App.css'
-import { useState } from 'react'
-import Board from './components/Board'
-import { Toaster } from 'react-hot-toast'
-import Joyride from "react-joyride";
-import { tutorialSteps } from './tutorialSteps';
-import { SaveDataInLocalStorage } from './localStorage';
 
 function App() {
-  const [ boardName, setBoardName ] = useState("Tablero básico")
-  SaveDataInLocalStorage()
   return (
     <>
-      <Joyride 
-        steps={tutorialSteps} 
-        showProgress 
-        showSkipButton 
-        hideCloseButton
-        disableScrolling
-        continuous
-        locale={{
-          back: 'Volver', 
-          close: 'Cerrar', 
-          last: 'Anterior', 
-          next: 'Siguiente', 
-          skip: 'Saltar tutorial'
-        }}
-        styles={{
-          options: {
-            backgroundColor: "#f3f3f3",
-            overlayColor: "rgb(0 0 0 / 46%)",
-            textColor: "#000",
-            primaryColor: "rgb(136,163,253)",
-            width: "18rem",
-            zIndex: 1
-          },
-        }}
-      />
-      <Toaster
-        position="bottom-right"
-        reverseOrder={false}
-      />
-      <Board name={boardName} changeName={setBoardName} />
     </>
   )
 }
--- src/components/atomic/colors.ts
+++
@@ -1,7 +0,0 @@
-
-export enum COLORS_CLASS_NAME {
-    PRIMARY = 'info',
-    DANGER = 'danger',
-    SUCCESS = 'success',
-    WARNING = 'warning'
-}
