5a4df7d981683909a2d96885ac2a2e78aebe1712
c.mirkin <ciromirkin@gmail.com>
2024-02-15 10:09:28 -0300

    eliminado: funciones y opciones de dominio

--- src/domainFunctions/addColumn.test.ts
+++
@@ -1,8 +0,0 @@
-import { getColumn } from "./addColumn"
-
-describe('Añadir una columna a la tabla', () => {
-    test('Se debería añadir una nueva columna al final de la tabla', () => {
-        const columns = [{name: "En espera", id: "1", taskList: []}]
-        expect(getColumn({ columnName: 'Para revision', columns })).toEqual({name: "Para revision", id: "2", taskList: []})
-    })
-})
