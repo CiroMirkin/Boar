d2eafdd83c6bbc133a6dcd768e7b7842b05b7ab7
c.mirkin <ciromirkin@gmail.com>
2024-02-15 11:33:10 -0300

    cambio: se movio el componente TaskList al archivo Task.tsx

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,4 +1,6 @@
+import React from "react";
 import { taskModel } from "../models/task";
+import { BoardDataContext } from "./Board";
 
 interface TaskProps {
     data: taskModel
@@ -9,5 +11,27 @@ export function Task({ data }: TaskProps) {
         <div>
             <p>{ data.descriptionText }</p>
         </div>
+    )
+}
+
+interface TaskListProps {
+    columnPosition: string
+}
+
+export function TaskList({ columnPosition }: TaskListProps) {
+    const columnIndex = (Number(columnPosition) - 1)
+    const tasks = React.useContext(BoardDataContext).tasksInColumns[columnIndex]
+    const taskList: React.ReactNode[] = []
+
+    tasks.forEach(task => {
+        taskList.push(
+            <Task data={task} key={task.id} />
+        )
+    })
+
+    return (
+        <>
+            {taskList}
+        </>
     )
 }
