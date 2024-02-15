c9c1e5d1eff0be9d14f83fccce942977dac7323b
c.mirkin <ciromirkin@gmail.com>
2024-02-15 11:32:07 -0300

    cambio: se movio el componente ColumnList al archivo Column.tsx

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { columnModel } from "../models/column";
+import { BoardDataContext } from "./Board";
+import { TaskList } from "./TaskList";
 
 interface ColumnProps {
     data: columnModel
@@ -11,6 +13,25 @@ export function Column({ data, children }: ColumnProps) {
         <div>
             <h3>{ data.name }</h3>
             { children }
+        </div>
+    )
+}
+
+export function ColumnList({ }) {
+    const columns = React.useContext(BoardDataContext).columnList
+    const columnList: React.ReactNode[] = []
+
+    columns.forEach(column => {
+        columnList.push(
+            <Column data={column} key={column.id}>
+                <TaskList columnPosition={column.position} />
+            </Column>
+        )
+    })
+
+    return (
+        <div>
+            {columnList}
         </div>
     )
 }
