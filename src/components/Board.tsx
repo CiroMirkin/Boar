432dbb1c9756a94cbc38cecdf54323128f76e490
c.mirkin <ciromirkin@gmail.com>
2024-02-16 10:27:53 -0300

    creacion: context para compartir una funcion para actualizar la informacion del tablero

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -3,6 +3,7 @@ import { boardModel, defaultBoard } from "../models/board"
 import { ColumnList } from "./Column"
 
 export const BoardDataContext = React.createContext(defaultBoard as boardModel)
+export const UpdateBoardData = React.createContext((newBoardData: boardModel): void => console.log('the set function is not defined, your new board data should be ', newBoardData))
 
 interface BoardProps {
     data: boardModel,
@@ -10,13 +11,16 @@ interface BoardProps {
 }
 
 export function Board({ data }: BoardProps) {
-    const [ allBoardData, setAllBoardData ] = useState(data)
-    console.log(data)
+    const [ allBoardData, setAllBoardData ] = useState(data as boardModel)
+    const updateAllBoardData = (newBoardData: boardModel) => setAllBoardData(newBoardData)
+
     return (
         <>
-        <h1>{ data.boardData.name }</h1>
+        <h1>{ allBoardData.boardData.name }</h1>
         <BoardDataContext.Provider value={allBoardData}>
-            <ColumnList />
+            <UpdateBoardData.Provider value={updateAllBoardData}>
+                <ColumnList />
+            </UpdateBoardData.Provider>
         </BoardDataContext.Provider>
         </>
     )
