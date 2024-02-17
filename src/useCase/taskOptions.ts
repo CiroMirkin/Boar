40f7e682a85ef2448615382719c8d1bbac2dd83b
c.mirkin <ciromirkin@gmail.com>
2024-02-17 10:45:34 -0300

    agregado: opcion de crear una tarea al arreglo de opciones

--- src/useCase/taskOptions.ts
+++ src/useCase/taskOptions.ts
@@ -1,5 +1,11 @@
 import { action } from "../models/option";
+import { addTaskInFirstColumn } from "./addTaskInFirstColumn";
 
 export const taskOptions: action[] = [
-    
+    {
+        id: '1',
+        name: 'Crear tarea',
+        enable: true,
+        action: ({ task, board }) => addTaskInFirstColumn({ task, board })
+    }
 ]
