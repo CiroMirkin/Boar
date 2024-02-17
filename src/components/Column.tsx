b839b2b4e1e922944bd7dfab4211bb6fb0305175
c.mirkin <ciromirkin@gmail.com>
2024-02-17 11:05:09 -0300

    agregado: formulario en la primer columna para crear tareas

--- src/components/Column.tsx
+++ src/components/Column.tsx
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import { columnModel } from "../models/column";
-import { AllBoardData } from "./Board";
+import { AllBoardData, UpdateBoardData } from "./Board";
 import { TaskList } from "./Task";
+import { isThisTheFirstColumn } from "../auxiliaryFunction/firstOrLastColumn";
+import { taskOptions } from "../useCase/taskOptions";
 
 export function ColumnList({ }) {
     const columns = React.useContext(AllBoardData).columnList
@@ -28,10 +30,26 @@ interface ColumnProps {
 }
 
 export function Column({ data, children }: ColumnProps) {
+    const [ newTaskDescription, setNewTaskDescription ] = useState('')
+    const handleClick = () => {
+        console.log(newTaskDescription)
+        setNewTaskDescription('')
+    }
+
     return (
         <div>
             <h3>{ data.name }</h3>
             { children }
+            {
+                isThisTheFirstColumn(data) 
+                && <footer>
+                    <input 
+                        type="text" value={newTaskDescription} 
+                        onChange={(e) => setNewTaskDescription(e.target.value)}  
+                    />
+                    <button onClick={handleClick}>Añadir</button>
+                </footer>
+            }
         </div>
     )
 }
