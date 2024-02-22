e5a1c101bdab153fd8f6a909b197142db11e1098
c.mirkin <ciromirkin@gmail.com>
2024-02-22 12:03:38 -0300

    agregado: se implemento la funcion para eliminar una tarea de su columna

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,13 +1,26 @@
+import React from "react";
 import { taskModel } from "../models/task";
+import { UpdateBoardData } from "./Board";
+import { deleteThisTask } from "../useCase/deleteTask";
 
 interface TaskProps {
     data: taskModel
 }
 
 export function Task({ data }: TaskProps) {
+    const updateBoard = React.useContext(UpdateBoardData)
+
+    const handleClick = () => {
+        updateBoard({
+            action: deleteThisTask,
+            task: data
+        })
+    }
+
     return (
         <div>
             <p>{ data.descriptionText }</p>
+            <button onClick={handleClick}>Eliminar</button>
         </div>
     )
 }
