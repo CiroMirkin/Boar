3da4f6eb7d0cfd0e5c8b3b0297327da70c1837d9
c.mirkin <ciromirkin@gmail.com>
2024-02-22 11:20:25 -0300

    cambio: se movio la lista de tareas dentro al interior de las columnas en el modelo del tablero

--- src/models/column.ts
+++ src/models/column.ts
@@ -1,23 +1,29 @@
+import { taskList } from "./task"
+
 export interface columnModel {
     id: string
     position: string,
     name: string
+    taskList: taskList
 }
 
 export const defaultColumnList: columnModel[] = [
     {
       id: "c1",
       name: "Pendientes",
-      position: "1"
+      position: "1",
+      taskList: []
     },
     {
       id: "c2",
       name: "Procesando",
       position: "2",
+      taskList: []
     },
     {
       id: "c3",
       name: "Terminado",
       position: "3",
+      taskList: []
     },
 ]
