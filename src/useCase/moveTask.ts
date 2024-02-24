82a63013c7d4c83b66c704a0d101a1a2fa9fc616
c.mirkin <ciromirkin@gmail.com>
2024-02-24 10:03:13 -0300

    eliminado: codigo innecesario

--- src/useCase/moveTask.ts
+++ src/useCase/moveTask.ts
@@ -15,9 +15,6 @@ export const moveThisTask = ({ task, to, board }: moveTaskParams): boardModel =>
     const newBoard = getCopyOfTheBoardData(board)
     const newColumns = newBoard.columnList
     const indexOfTheColumnWhereTheTaskIs: number = getIndexOfColumnInColumnList(task.columnPosition);
-    // Este arreglo es necesario porque la referencia de las taskList dentro de las columnas no cambia
-    // Por ende cuando se elimina la tarea se elimina de newBoard y de board
-    const oldColumnTaskList = [...newBoard.columnList[indexOfTheColumnWhereTheTaskIs].taskList]
 
     newBoard.columnList[indexOfTheColumnWhereTheTaskIs].taskList = newBoard.columnList[indexOfTheColumnWhereTheTaskIs].taskList.filter(t => t.id !== task.id)
 
@@ -32,7 +29,6 @@ export const moveThisTask = ({ task, to, board }: moveTaskParams): boardModel =>
         return newBoard
     } 
 
-    board.columnList[indexOfTheColumnWhereTheTaskIs].taskList = oldColumnTaskList
     return board
 }
 
