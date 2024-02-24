a575e4fbd0eba827cc5226bc3c8396259fdf2871
c.mirkin <ciromirkin@gmail.com>
2024-02-24 10:02:28 -0300

    refactor: se hizo mas eficiente la funcion getCopyOfTheBoardData

--- src/auxiliaryFunction/copyBoardData.ts
+++ src/auxiliaryFunction/copyBoardData.ts
@@ -1,9 +1,5 @@
 import { boardModel } from "../models/board"
 
 export const getCopyOfTheBoardData = (allBoardData: boardModel): boardModel => {
-    const { boardData, columnList } = allBoardData
-    return {
-        boardData: {...boardData},
-        columnList: [...columnList],
-    }
+    return structuredClone(allBoardData)
 }
