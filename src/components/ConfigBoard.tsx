d23a52e6a12f8bf262fe9289341a145116a40184
c.mirkin <ciromirkin@gmail.com>
2024-03-02 11:20:06 -0300

    agregado: se muestra el nombre del tablero en el componente ConfigBoard

--- src/components/ConfigBoard.tsx
+++ src/components/ConfigBoard.tsx
@@ -1,10 +1,17 @@
+import React from "react"
+import { AllBoardData } from "../App"
 
 interface ConfigBoardParams {}
 
 export function ConfigBoard({}:ConfigBoardParams) {
+    const boardData = React.useContext(AllBoardData)
+
     return (
         <>
             <h1>Preferencias</h1>
+            <div>
+                <h2>{boardData.boardData.name}</h2>
+            </div>
         </>
     )
 }
