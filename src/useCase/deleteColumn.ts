70635fbd4d1315e3f6e0b59e720f44d295d3ee68
c.mirkin <ciromirkin@gmail.com>
2024-03-07 10:58:15 -0300

    agregado: codigo para que la funcion de eliminar columnas pase sus pruebas

--- src/useCase/deleteColumn.ts
+++ src/useCase/deleteColumn.ts
@@ -3,5 +3,7 @@ import { boardModel, columnActionParams } from "../models/board";
 
 export function deleteThisColumn({ board, column }: columnActionParams): boardModel {
     const newBoard = getCopyOfTheBoardData(board) 
+    const newColumns = newBoard.columnList.filter(columnInBoard => columnInBoard.id !== column.id)
+    newBoard.columnList = newColumns
     return newBoard
 }
