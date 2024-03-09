cba8d35b7509bb58a3d8d54bc37830e9312ca5d2
c.mirkin <ciromirkin@gmail.com>
2024-03-09 10:17:03 -0300

    agregado: codigo a la funcion delteThisColumn para pasar todas sus pruebas

--- src/useCase/deleteColumn.ts
+++ src/useCase/deleteColumn.ts
@@ -3,6 +3,8 @@ import { boardModel } from "../models/board";
 import { columnUseCaseParams } from "./useCase";
 
 export function deleteThisColumn({ board, column }: columnUseCaseParams): boardModel {
+    if(board.columnList.length <= 3) throw('Para poder eliminar una columna debe haber mas de tres.')
+    
     const newBoard = getCopyOfTheBoardData(board) 
     const newColumns = newBoard.columnList.filter(columnInBoard => columnInBoard.id !== column.id)
     newBoard.columnList = newColumns
