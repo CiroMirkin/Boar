af131cc1a3137991062f4fba21a93bb4231753cb
c.mirkin <ciromirkin@gmail.com>
2024-03-09 09:55:39 -0300

    agregado: codigo necesario para que la funcion addColumnAtTheEnd pase nuevamente sus pruebas

--- src/useCase/createColumn.ts
+++ src/useCase/createColumn.ts
@@ -5,6 +5,9 @@ import { columnModel } from "../models/column";
 
 export function addColumnAtTheEnd({ board, column }: columnUseCaseParams): boardModel {
     const newBoard = getCopyOfTheBoardData(board)
+    if(column.position === '-1') {
+        column.position = JSON.stringify(newBoard.columnList.length + 1)
+    }
     newBoard.columnList.push(column)
     return newBoard
 }
