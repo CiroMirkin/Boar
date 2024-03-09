902d3745c8f8b4652f924f4692f74ef2f0456930
c.mirkin <ciromirkin@gmail.com>
2024-03-09 09:50:33 -0300

    creado: funcion createColumnWithoutPosition

--- src/useCase/createColumn.ts
+++ src/useCase/createColumn.ts
@@ -1,9 +1,19 @@
 import { getCopyOfTheBoardData } from "../auxiliaryFunction/copyBoardData";
 import { columnUseCaseParams } from "./useCase";
 import { boardModel } from "../models/board";
+import { columnModel } from "../models/column";
 
 export function addColumnAtTheEnd({ board, column }: columnUseCaseParams): boardModel {
     const newBoard = getCopyOfTheBoardData(board)
     newBoard.columnList.push(column)
     return newBoard
+}
+
+export function createColumnWithoutPosition({ name }: { name: string }): columnModel {
+    return {
+        id: crypto.randomUUID(),
+        position: '-1',
+        name,
+        taskList: []
+    }
 }
