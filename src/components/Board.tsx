02dd175d3b9209ba5c5637ffcd742a6e88585536
c.mirkin <ciromirkin@gmail.com>
2024-03-14 11:33:50 -0300

    implementado: separacion entre la lista de columnas y el contenido de las columnas a nivel componente

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { boardModel } from "../models/board"
 import { ColumnList } from "./ColumnList"
+import { TaskList } from "./TaskList"
 
 interface BoardProps {
     data: boardModel,
@@ -8,10 +9,16 @@ interface BoardProps {
 }
 
 export function Board({ data }: BoardProps) {
+    const columnsContent: React.ReactNode[] = []
+    data.columnList.forEach(column => {
+        columnsContent.push(
+            <TaskList tasks={column.taskList} />
+        )
+    })
     return (
         <>
         <h1>{ data.boardData.name }</h1>
-        <ColumnList />
+        <ColumnList columnsContent={columnsContent} />
         </>
     )
 }
--- src/components/ColumnList.tsx
+++ src/components/ColumnList.tsx
@@ -1,19 +1,14 @@
 import React from "react";
 import { BoardData } from "../App";
-import { TaskList } from "./TaskList";
 import { Column } from "./Column";
 import './ColumnList.css'
 
-export function ColumnList({ }) {
+interface ColumnListProps {
+    columnsContent: React.ReactNode[]
+}
+export function ColumnList({ columnsContent }: ColumnListProps) {
     const columns = React.useContext(BoardData).board.columnList;
     const columnList: React.ReactNode[] = [];
-    const columnsContent: React.ReactNode[] = []
-
-    columns.forEach(column => {
-        columnsContent.push(
-            <TaskList tasks={column.taskList} />
-        )
-    })
 
     columns.forEach((column, columnIndex) => {
         columnList.push(
