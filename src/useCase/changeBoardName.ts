ef127900a11a411b2748b02cc2565aff3d4404c3
c.mirkin <ciromirkin@gmail.com>
2024-03-14 11:12:13 -0300

    eliminado: codigo llamada a la funcion getCopyOfTheBoardData

--- src/useCase/changeBoardName.ts
+++ src/useCase/changeBoardName.ts
@@ -1,9 +1,8 @@
-import { getCopyOfTheBoardData } from "../utility/copyBoardData";
 import { boardModel } from "../models/board";
 import { changeNameParams } from "./useCase";
 
 export function changeBoardName({ board, newName }: changeNameParams): boardModel {
-    const newBoard = getCopyOfTheBoardData(board)
+    const newBoard = board
     newBoard.boardData.name = newName
     return newBoard
 }
