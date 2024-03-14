bc552d175f0d0d4fca809a9e69ab2e9c5d1ed3b2
c.mirkin <ciromirkin@gmail.com>
2024-03-14 11:12:59 -0300

    eliminado: codigo llamada a la funcion getCopyOfTheBoardData

--- src/useCase/deleteColumn.ts
+++ src/useCase/deleteColumn.ts
@@ -1,11 +1,10 @@
-import { getCopyOfTheBoardData } from "../utility/copyBoardData";
 import { boardModel } from "../models/board";
 import { columnUseCaseParams } from "./useCase";
 
 export function deleteThisColumn({ board, column }: columnUseCaseParams): boardModel {
     if(board.columnList.length <= 3) throw('Para poder eliminar una columna debe haber mas de tres.')
     
-    const newBoard = getCopyOfTheBoardData(board) 
+    const newBoard = board
     const newColumns = newBoard.columnList.filter(columnInBoard => columnInBoard.id !== column.id)
     newBoard.columnList = newColumns
     return newBoard
