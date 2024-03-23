fae57048dc3d1629242bc39c935efae4c7e161fa
c.mirkin <ciromirkin@gmail.com>
2024-03-23 11:08:23 -0300

    cambio: bateria de pruebas de la funcion archiveTakListInColumn

--- src/useCase/archiveTaskList.test.ts
+++ src/useCase/archiveTaskList.test.ts
@@ -1,77 +1,17 @@
-import { taskNull } from "../models/task"
+import { taskList, taskNull } from "../models/task"
 import { archiveTaskListInColumn } from "./archiveTaskList"
 import { getFullDate } from "../utility/getTime"
 
 describe("Archivar lista de tareas.", () => {
     test("Se deberían archivar todas las tareas de la columna indicada.", () => {
-        const task = {...taskNull}
-        const board = {
-            boardData: {
-                id: '0',
-                name: "Tablero básico"
-            },
-            columnList: [
-                {
-                    id: "c1",
-                    name: "Pendientes",
-                    position: "1",
-                    taskList: []
-                },
-                {
-                    id: "c2",
-                    name: "Procesando",
-                    position: "2",
-                    taskList: []
-                },
-                {
-                    id: "c3",
-                    name: "Terminado",
-                    position: "3",
-                    taskList: [{...task}]
-                },
-            ],
-            archive: []
-        }
-        expect(archiveTaskListInColumn({ board, columnIndex: 2 })).toStrictEqual(
+        const taskListInEachColumn: taskList[] = [[{...taskNull}]]
+        expect(archiveTaskListInColumn({ taskListInEachColumn, columnPosition: '1', archive: [] })).toStrictEqual([
             {
-                boardData: {
-                    id: '0',
-                    name: "Tablero básico"
-                },
-                columnList: [
-                    {
-                        id: "c1",
-                        name: "Pendientes",
-                        position: "1",
-                        taskList: []
-                    },
-                    {
-                        id: "c2",
-                        name: "Procesando",
-                        position: "2",
-                        taskList: []
-                    },
-                    {
-                        id: "c3",
-                        name: "Terminado",
-                        position: "3",
-                        taskList: []
-                    },
-                ],
-                archive: [
-                    {
-                        date: (getFullDate()),
-                        tasklist: [
-                            {
-                                id: '',
-                                descriptionText: '',
-                                columnPosition: '1',
-                                highlight: false
-                            }
-                        ]
-                    }
+                date: (getFullDate()),
+                tasklist: [
+                    {...taskNull}
                 ]
             }
-        )
+        ])
     })
 })
