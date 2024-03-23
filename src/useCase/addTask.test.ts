b0461b8245822dde920747a2f89a3ad5757447ff
c.mirkin <ciromirkin@gmail.com>
2024-03-23 09:37:44 -0300

    actualizado: bateria de pruebas de la funcion addTaskInFirstColumn

--- src/useCase/addTask.test.ts
+++ src/useCase/addTask.test.ts
@@ -1,44 +1,24 @@
-import { defaultBoard } from "../models/board"
+import { taskList } from "@/models/task"
 import { addTaskInFirstColumn } from "./addTask"
 
 describe("Crear una tarea.", () => {
     test("Se debería agregar la tarea recibida a la primer columna del tablero.", () => {
-        const board = defaultBoard
         const task = {
             id: '',
             descriptionText: '',
             columnPosition: '1',
         }
-        expect(addTaskInFirstColumn({ board, task })).toStrictEqual({
-            boardData: {
-                id: '0',
-                name: "Tablero básico"
-            },
-            columnList: [
+        const taskList: taskList[] = [ [], [], [] ]
+        expect(addTaskInFirstColumn({ taskList, task })).toStrictEqual([ 
+            [
                 {
-                    id: "c1",
-                    name: "Pendientes",
-                    position: "1",
-                    taskList: [{
-                        id: '',
-                        descriptionText: '',
-                        columnPosition: '1',
-                    }]
-                },
-                {
-                    id: "c2",
-                    name: "Procesando",
-                    position: "2",
-                    taskList: []
-                },
-                {
-                    id: "c3",
-                    name: "Terminado",
-                    position: "3",
-                    taskList: []
-                },
-            ],
-            archive: []
-        })
+                    id: '',
+                    descriptionText: '',
+                    columnPosition: '1',
+                }
+            ], 
+            [], 
+            [] 
+        ])
     })
 })
