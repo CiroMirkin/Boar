b57b7bae077b67515df64ac14dc8d8b004267f05
c.mirkin <ciromirkin@gmail.com>
2024-03-23 10:53:16 -0300

    actualizado: funcion deleteThisColumn

--- src/useCase/column/deleteColumn.ts
+++ src/useCase/column/deleteColumn.ts
@@ -1,11 +1,8 @@
-import { boardModel } from "../../models/board";
+import { columnModel } from "@/models/column";
 import { columnUseCaseParams } from "../useCase";
 
-export function deleteThisColumn({ board, column }: columnUseCaseParams): boardModel {
-    if(board.columnList.length <= 3) throw('Para poder eliminar una columna debe haber mas de tres.')
-    
-    const newBoard = board
-    const newColumns = newBoard.columnList.filter(columnInBoard => columnInBoard.id !== column.id)
-    newBoard.columnList = newColumns
-    return newBoard
+export function deleteThisColumn({ columnList, column }: columnUseCaseParams): columnModel[] {
+    if(columnList.length <= 3) throw('Para poder eliminar una columna debe haber mas de tres.')
+
+    return columnList.filter(columnInBoard => columnInBoard.id !== column.id)
 }
