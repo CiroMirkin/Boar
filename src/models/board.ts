13a048fde39033bebdeeb7598fff85ac096982e5
c.mirkin <ciromirkin@gmail.com>
2024-03-23 09:28:20 -0300

    cambio: modelo del tablero

--- src/models/board.ts
+++ src/models/board.ts
@@ -1,29 +1,4 @@
-import { getCopyOfTheBoardData } from "../utility/copyBoardData";
-import { taskListArchived } from "./archive";
-import { columnModel, defaultColumnList } from "./column";
-
-export interface boardData {
+export interface boardModel {
     id: string,
     name: string
-}
-
-export interface boardModel {
-    boardData: boardData,
-    columnList: columnModel[],
-    archive: taskListArchived[]
-}
-  
-export const defaultBoard: boardModel = {
-    boardData: {
-        id: '0',
-        name: "Tablero básico"
-    },
-    columnList: defaultColumnList,
-    archive: []
-};
-
-export const getNewBlankBoard = (): boardModel => {
-    const newDefaultBoard = getCopyOfTheBoardData(defaultBoard)
-    newDefaultBoard.boardData.id = crypto.randomUUID()
-    return newDefaultBoard
 }
