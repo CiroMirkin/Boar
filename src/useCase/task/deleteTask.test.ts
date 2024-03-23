611364b3fbe2c7db499d9a7bcde41cf791b794c0
c.mirkin <ciromirkin@gmail.com>
2024-03-23 09:49:03 -0300

    actualizado: bateria de pruebas de la funcion deleteThisTask

--- src/useCase/task/deleteTask.test.ts
+++ src/useCase/task/deleteTask.test.ts
@@ -1,67 +1,15 @@
 import { deleteThisTask } from "./deleteTask"
 
 describe("Eliminar una tarea.", () => {
-    test("Se debería eliminar la tarea elegida de su columna.", () => {
-        const board = {
-            boardData: {
-                id: '0',
-                name: "Tablero básico"
-            },
-            columnList: [
-                {
-                    id: "c1",
-                    name: "Pendientes",
-                    position: "1",
-                    taskList: [{
-                        id: '',
-                        descriptionText: '',
-                        columnPosition: '1',
-                    }]
-                },
-                {
-                    id: "c2",
-                    name: "Procesando",
-                    position: "2",
-                    taskList: []
-                },
-                {
-                    id: "c3",
-                    name: "Terminado",
-                    position: "3",
-                    taskList: []
-                },
-            ]
-        }
+    test("Se debería eliminar la tarea elegida de su columna.", () => {       
         const task = {
             id: '',
             descriptionText: '',
             columnPosition: '1',
         }
-        expect(deleteThisTask({ board, task })).toStrictEqual({
-            boardData: {
-                id: '0',
-                name: "Tablero básico"
-            },
-            columnList: [
-                {
-                    id: "c1",
-                    name: "Pendientes",
-                    position: "1",
-                    taskList: []
-                },
-                {
-                    id: "c2",
-                    name: "Procesando",
-                    position: "2",
-                    taskList: []
-                },
-                {
-                    id: "c3",
-                    name: "Terminado",
-                    position: "3",
-                    taskList: []
-                },
-            ]
-        })
+        const taskListInEachColumn = [
+            [ {...task} ], [], []
+        ]
+        expect(deleteThisTask({ taskListInEachColumn, task })).toStrictEqual([ [], [], [] ])
     })
 })
