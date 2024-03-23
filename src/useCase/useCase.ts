966ac28b793f4734010700eaf077d0d7d7f59bec
c.mirkin <ciromirkin@gmail.com>
2024-03-23 09:30:43 -0300

    cambio: interfaz taskUseCaseParams

--- src/useCase/useCase.ts
+++ src/useCase/useCase.ts
@@ -1,12 +1,13 @@
 import { boardModel } from "../models/board";
 import { columnModel } from "../models/column";
-import { taskModel } from "../models/task";
+import { taskList, taskModel } from "../models/task";
 
 
 export interface boardUseCaseParams {
     board: boardModel;
 }
-export interface taskUseCaseParams extends boardUseCaseParams {
+export interface taskUseCaseParams {
+    taskList: taskList;
     task: taskModel;
 }
 export interface changeNameParams extends boardUseCaseParams {
