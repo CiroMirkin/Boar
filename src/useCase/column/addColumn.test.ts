ec74f22f0ac34113e2527f963c9264639ab0a46c
c.mirkin <ciromirkin@gmail.com>
2024-03-23 10:36:46 -0300

    cambio: bateria de pruebas de la funcion addCoumnAtTheEnd

--- src/useCase/column/addColumn.test.ts
+++ src/useCase/column/addColumn.test.ts
@@ -3,116 +3,57 @@ import { addColumnAtTheEnd } from "./addColumn"
 
 describe('Crear una columna.', () => {
     test('Se debería añadir una columna al final del tablero.', () => {
-        const board = {
-            boardData: {
-                id: '0',
-                name: "Tablero básico"
-            },
-            columnList: [
-                {
-                    id: "",
-                    name: "",
-                    position: "1",
-                    taskList: []
-                },
-                {
-                    id: "",
-                    name: "",
-                    position: "2",
-                    taskList: []
-                },
-                {
-                    id: "",
-                    name: "",
-                    position: "3",
-                    taskList: []
-                }
-            ],
-            archive: []
-            }
-        const column: columnModel = {
-            id: '',
-            position: '4',
-            name: '',
-            taskList: []
-        }
-      expect(addColumnAtTheEnd({ board, column })).toStrictEqual({
-        boardData: {
-            id: '0',
-            name: "Tablero básico"
-        },
-        columnList: [
+        const columnList = [
             {
                 id: "",
                 name: "",
                 position: "1",
-                taskList: []
+            }
+        ]
+        const column: columnModel = {
+            id: "",
+            name: "",
+            position: "2",
+        }
+      expect(addColumnAtTheEnd({ columnList, column })).toStrictEqual([
+            {
+                id: "",
+                name: "",
+                position: "1",
             },
             {
                 id: "",
                 name: "",
                 position: "2",
-                taskList: []
+            }
+        ])  
+    })
+    
+    test('Se debería corregir la posición de una columna si esta no lo tiene.', () => {
+        const columnList = [
+            {
+                id: "",
+                name: "",
+                position: "1",
+
+            }
+        ]
+        const column: columnModel = {
+            id: "",
+            name: "",
+            position: "-1"
+        }
+        expect(addColumnAtTheEnd({ columnList, column })).toStrictEqual([
+            {
+                id: "",
+                name: "",
+                position: "1",
             },
             {
                 id: "",
                 name: "",
-                position: "3",
-                taskList: []
-            },
-            {
-                id: '',
-                position: '4',
-                name: '',
-                taskList: []
+                position: "2",
             }
-        ],
-        archive: []
-        })  
-    })
-    
-    test('Se debería corregir la posición de una columna si esta no lo tiene.', () => {
-        const board = {
-            boardData: {
-                id: '',
-                name: ""
-            },
-            columnList: [
-                {
-                    id: "",
-                    name: "",
-                    position: "1",
-                    taskList: []
-                }
-            ],
-            archive: []
-        }
-        const column: columnModel = {
-            id: "",
-            name: "",
-            position: "-1",
-            taskList: []
-        }
-        expect(addColumnAtTheEnd({ board, column })).toStrictEqual({
-            boardData: {
-                id: '',
-                name: ""
-            },
-            columnList: [
-                {
-                    id: "",
-                    name: "",
-                    position: "1",
-                    taskList: []
-                },
-                {
-                    id: "",
-                    name: "",
-                    position: "2",
-                    taskList: []
-                }
-            ],
-            archive: []
-        })
+        ])
     })
 })
