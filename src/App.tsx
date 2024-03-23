d974f23a69985954f1b85a88869f5718957c27d1
c.mirkin <ciromirkin@gmail.com>
2024-03-23 12:50:22 -0300

    creado: contexto global para obtener el redux columnListSlice

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import React from 'react'
 import './App.css'
 import { Board } from './components/Board'
+import { defaultColumnList } from './models/column'
+import { useSelector } from 'react-redux'
+import { RootState } from './redux/store'
+
+export const ColumnList = React.createContext(defaultColumnList)
 
 function App() {
+  const columnList = useSelector((state: RootState) => state.columnList)
   return (
     <>
+    <ColumnList.Provider value={columnList}>
       <Board />
+    </ColumnList.Provider>
     </>
   )
 }
