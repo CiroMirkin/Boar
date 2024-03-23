b6058eeeec54f366badf332306fb8d1c8619a2d5
c.mirkin <ciromirkin@gmail.com>
2024-03-23 09:40:01 -0300

    actualizado: funcion addTaskInFirstColumn para que pase su nueva bateria de pruebas

--- src/useCase/addTask.ts
+++ src/useCase/addTask.ts
@@ -1,9 +1,8 @@
-import { boardModel } from "../models/board";
+import { taskList } from "@/models/task";
 import { taskUseCaseParams } from "./useCase";
 
-export function addTaskInFirstColumn({ taskList, task }: taskUseCaseParams): boardModel {
-    const columnIndex = 0
-    board.columnList[columnIndex].taskList.push(task)
-    const newBoard = board
-    return newBoard
+export function addTaskInFirstColumn({ taskList, task }: taskUseCaseParams): taskList[] {
+    const columnPosition = 0
+    taskList[columnPosition].push(task)
+    return taskList
 }
