c9170254d1705044108e3965f4e6321061f3e682
c.mirkin <ciromirkin@gmail.com>
2024-03-23 10:43:33 -0300

    cambio: bateria de pruebas de la funcion changeNameOfColumn

--- src/useCase/column/changeColumnName.test.ts
+++ src/useCase/column/changeColumnName.test.ts
@@ -1,44 +1,22 @@
-import { boardModel } from "../../models/board"
 import { columnModel } from "../../models/column"
 import { changeNameOfColumn } from "./changeColumnName"
 
 describe('Cambiar el nombre de una columna.', () => {
-    test('Se debería cambiar el nombre de una columna.', () => { 
-        const board: boardModel = {
-            boardData: {
-                id: '',
-                name: ""
-            },
-            columnList: [
-                {
-                    id: "c1",
-                    position: "1",
-                    name: "",
-                    taskList: []
-                }
-            ],
-            archive: []
-        }
+    test('Se debería cambiar el nombre de la columna indicada.', () => { 
         const column: columnModel = {
             id: "c1",
             name: "",
             position: "1",
-            taskList: []
         }
-        expect(changeNameOfColumn({ board, column, newName: "pipi" })).toStrictEqual({
-                boardData: {
-                    id: '',
-                    name: ""
-                },
-            columnList: [
-                {
-                    id: "c1",
-                    position: "1",
-                    name: "pipi",
-                    taskList: []
-                }
-            ],
-            archive: []
-        })
+        const columnList: columnModel[] = [
+            {...column}
+        ]
+        expect(changeNameOfColumn({ columnList, column, newName: "pipi" })).toStrictEqual([
+            {
+                id: "c1",
+                position: "1",
+                name: "pipi",
+            }
+        ])
     })
 })
