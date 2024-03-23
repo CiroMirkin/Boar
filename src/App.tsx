513d121e397613edc777c77b7c9cce2116b2896d
c.mirkin <ciromirkin@gmail.com>
2024-03-23 13:22:07 -0300

    creado: contexto para desacoplar el slice de redux taskListInEachColumn

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,20 @@ import { Board } from './components/Board'
 import { defaultColumnList } from './models/column'
 import { useSelector } from 'react-redux'
 import { RootState } from './redux/store'
+import { taskList } from './models/task'
 
 export const ColumnListContext = React.createContext(defaultColumnList)
+export const TaskListInEachColumnContext = React.createContext([[], [], []] as taskList[])
 
 function App() {
   const columnList = useSelector((state: RootState) => state.columnList)
+  const taskListInEachColumn = useSelector((state: RootState) => state.taskListInEachColumn)
   return (
     <>
     <ColumnListContext.Provider value={columnList}>
+      <TaskListInEachColumnContext.Provider value={taskListInEachColumn}>
       <Board />
+      </TaskListInEachColumnContext.Provider>
     </ColumnListContext.Provider>
     </>
   )
