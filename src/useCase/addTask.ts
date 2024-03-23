fa6fcc93492be930d06b6d6e71b5932cab091281
c.mirkin <ciromirkin@gmail.com>
2024-03-23 09:36:23 -0300

    actualizado: parametros de la funcion addTaskInFirstColumn

--- src/useCase/addTask.ts
+++ src/useCase/addTask.ts
@@ -1,7 +1,7 @@
 import { boardModel } from "../models/board";
 import { taskUseCaseParams } from "./useCase";
 
-export function addTaskInFirstColumn({ board, task }: taskUseCaseParams): boardModel {
+export function addTaskInFirstColumn({ taskList, task }: taskUseCaseParams): boardModel {
     const columnIndex = 0
     board.columnList[columnIndex].taskList.push(task)
     const newBoard = board
