62493166ec6ed53f20e08c3a05c4ec355e0140dd
c.mirkin <ciromirkin@gmail.com>
2024-03-24 11:05:00 -0300

    agregado: variante al componente Button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -17,6 +17,7 @@ const buttonVariants = cva(
         secondary:
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
+        destructiveGhost: "hover:bg-destructive hover:text-destructive-foreground",
         link: "text-primary underline-offset-4 hover:underline",
       },
       size: {
