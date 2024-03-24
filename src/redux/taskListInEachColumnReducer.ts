438efdd97a505da96ac8f1f1bbdc30e2680a6b4a
c.mirkin <ciromirkin@gmail.com>
2024-03-24 10:45:17 -0300

    creado: dos nuevos reducers uno para avanzar una tarea y otro para retrocederla

--- src/redux/taskListInEachColumnReducer.ts
+++ src/redux/taskListInEachColumnReducer.ts
@@ -1,6 +1,7 @@
 import { taskList, taskModel } from "@/models/task";
 import { addTaskInFirstColumn } from "@/useCase/task/addTask";
 import { deleteThisTask } from "@/useCase/task/deleteTask";
+import { moveThisTaskToTheNextColumn, moveThisTaskToThePrevColumn } from "@/useCase/task/moveTask";
 import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
 interface InitialState {
@@ -22,9 +23,17 @@ export const taskListInEachColumnSlice = createSlice({
         deleteTask: (state, action: PayloadAction<taskModel>) => {
             const task = action.payload
             state.list = deleteThisTask({ taskListInEachColumn: state.list, task })
+        },
+        moveTaskToNextColumn: (state, action: PayloadAction<taskModel>) => {
+            const task = action.payload
+            state.list = moveThisTaskToTheNextColumn({ taskListInEachColumn: state.list, task })
+        },
+        moveTaskToPrevColumn: (state, action: PayloadAction<taskModel>) => {
+            const task = action.payload
+            state.list = moveThisTaskToThePrevColumn({ taskListInEachColumn: state.list, task })
         }
     }
 })
 
-export const { addTaskAtFirstColumn, deleteTask } = taskListInEachColumnSlice.actions
+export const { addTaskAtFirstColumn, deleteTask, moveTaskToNextColumn, moveTaskToPrevColumn } = taskListInEachColumnSlice.actions
 export default taskListInEachColumnSlice.reducer
