4ed6b18159a845b0648afd92aa9e7f803c3e9f86
c.mirkin <ciromirkin@gmail.com>
2024-03-26 10:29:05 -0300

    creado: redux reducer para archivar tareas

--- src/redux/archiveReducer.ts
+++ src/redux/archiveReducer.ts
@@ -1,12 +1,20 @@
 import { archive } from "@/models/archive";
-import { createSlice } from "@reduxjs/toolkit";
+import { taskList } from "@/models/task";
+import { archiveTaskListInColumn } from "@/useCase/archiveTaskList";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
 const initialState: archive = []
 
 export const archiveSlice = createSlice({
     name: 'archive',
     initialState,
-    reducers: {}
+    reducers: {
+        archiveTaskListAtLastColumn: (state, action: PayloadAction<taskList[]>) => {
+            const taskList = action.payload
+            state = archiveTaskListInColumn({ taskListInEachColumn: taskList, columnPosition: "3" , archive: state })
+        },
+    }
 })
 
+export const { archiveTaskListAtLastColumn } = archiveSlice.actions
 export default archiveSlice.reducer
