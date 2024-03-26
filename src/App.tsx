6fe3f3d76f1e1ffdcb3e77ce08aa90329aa08b4b
c.mirkin <ciromirkin@gmail.com>
2024-03-26 18:59:31 -0300

    creado: funcion getArchive en el componente App

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,14 @@ import { defaultColumnList } from './models/column'
 import { useSelector } from 'react-redux'
 import { RootState } from './redux/store'
 import { taskList } from './models/task'
+import { archive } from './models/archive'
 
 export const ColumnListContext = React.createContext(defaultColumnList)
 export const TaskListInEachColumnContext = React.createContext([[], [], []] as taskList[])
+
+const getArchive = (): archive => {
+  return useSelector((state: RootState) => state.archive).reverse()
+}
 
 function App() {
   const columnList = useSelector((state: RootState) => state.columnList)
