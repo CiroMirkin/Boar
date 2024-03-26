76fa8a7e3bb0110ac8299d39b5f5b39dbff907b4
c.mirkin <ciromirkin@gmail.com>
2024-03-26 10:09:43 -0300

    agregado: clases de tailwing al componente Archive

--- src/components/Archive.tsx
+++ src/components/Archive.tsx
@@ -1,6 +1,7 @@
 import React from "react"
 import { Task } from "./Task"
 import { archive } from "@/models/archive"
+import { Card, CardContent, CardHeader, CardTitle } from "./ui/card"
 
 interface ArchiveProps { }
 
@@ -18,16 +19,24 @@ export function Archive({}: ArchiveProps) {
             )
         )
         archive.push(
-            <div key={date}>
-                <h3>{date}</h3>
-                {taskListArchived}
-            </div>
+            <Card key={date} className="p-2">
+                <CardHeader>
+                    <CardTitle>
+                        <h3>{date}</h3>
+                    </CardTitle>
+                </CardHeader>
+                <CardContent className="flex flex-col gap-y-2">
+                    {taskListArchived}
+                </CardContent>
+            </Card>
         )
     })
 
     return (
         <>
+            <div className="mx-6 my-4 flex flex-col gap-y-2">
             {archive}
+            </div>
         </>
     )
 }
