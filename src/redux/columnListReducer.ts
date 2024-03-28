47d06660f98b8199b760c5cb312777770bc04b77
c.mirkin <ciromirkin@gmail.com>
2024-03-28 10:17:18 -0300

    creado: el reducer addColumn y deleteColumn en el column slice

--- src/redux/columnListReducer.ts
+++ src/redux/columnListReducer.ts
@@ -1,12 +1,24 @@
-import { defaultColumnList } from "@/models/column";
-import { createSlice } from "@reduxjs/toolkit";
+import { columnModel, defaultColumnList } from "@/models/column";
+import { addColumnAtTheEnd } from "@/useCase/column/addColumn";
+import { deleteThisColumn } from "@/useCase/column/deleteColumn";
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
 
 const initialState = defaultColumnList
 
 export const columnListSlice = createSlice({
     name: 'columnList',
     initialState,
-    reducers: {}
+    reducers: {
+        addColumn: (state, action: PayloadAction<columnModel>) => {
+            const column = action.payload
+            state = addColumnAtTheEnd({ column, columnList: state})
+        },
+        deleteColumn: (state, action: PayloadAction<columnModel>) => {
+            const column = action.payload
+            state = deleteThisColumn({ column, columnList: state})
+        }
+    }
 })
 
+export const { addColumn, deleteColumn } = columnListSlice.actions
 export default columnListSlice.reducer
