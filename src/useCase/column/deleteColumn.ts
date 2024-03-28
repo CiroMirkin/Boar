876139f88a0911a10fd7153aae176ea70c43d4e9
c.mirkin <ciromirkin@gmail.com>
2024-03-28 11:04:32 -0300

    cambio: forma de lanzar errores en el caso de uso deleteColumn

--- src/useCase/column/deleteColumn.ts
+++ src/useCase/column/deleteColumn.ts
@@ -2,7 +2,7 @@ import { columnModel } from "@/models/column";
 import { columnUseCaseParams } from "../useCase";
 
 export function deleteThisColumn({ columnList, column }: columnUseCaseParams): columnModel[] {
-    if(columnList.length <= 3) throw('Para poder eliminar una columna debe haber mas de tres.')
+    if(columnList.length <= 3) throw new Error('Para poder eliminar una columna debe haber mas de tres.')
 
     return columnList.filter(columnInBoard => columnInBoard.id !== column.id)
 }
