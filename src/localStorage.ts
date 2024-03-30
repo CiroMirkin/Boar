415911ffbfadce44d1de9b59ae49b0416f8bc91f
c.mirkin <ciromirkin@gmail.com>
2024-03-30 12:57:57 -0300

    eliminado: archivo muerto

--- src/localStorage.ts
+++
@@ -1,9 +0,0 @@
-import { useSelector } from "react-redux";
-import { RootState } from "./redux/store";
-
-export const localStorageKey = 'boar-columns'
-
-export function SaveDataInLocalStorage (): void {
-    const columns = useSelector((state: RootState) => state.columns)
-    localStorage.setItem(localStorageKey, JSON.stringify(columns))
-}
