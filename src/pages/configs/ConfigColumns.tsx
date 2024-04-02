d35c6be77650f73f60be7985908f6843831a74d1
c.mirkin <ciromirkin@gmail.com>
2024-04-02 18:34:49 -0300

    creado: componente ConfigColumns

---
+++ src/pages/configs/ConfigColumns.tsx
@@ -0,0 +1,63 @@
+import React from "react"
+import { getBlankColumnWithoutPosition } from "../../models/column"
+import { columnModel } from "../../models/column"
+import { useDispatch } from "react-redux"
+import { addColumn, deleteColumn } from "@/redux/columnListReducer"
+import { Card, CardContent, CardHeader, CardTitle } from "../../ui/card"
+import { Button } from "../../ui/button"
+import { useToast } from "../../ui/use-toast"
+import { Plus, Trash2 } from "lucide-react"
+import { iconSize } from "@/iconsConstants"
+
+interface ConfigColumnsParams {
+    columnList: columnModel[]
+}
+
+export function ConfigColumns({ columnList }: ConfigColumnsParams) {
+    const updateBoardData = useDispatch()
+    const { toast } = useToast()
+    
+    const getNewColumn = () => getBlankColumnWithoutPosition({ name: 'Nueva columna'})
+
+    const handleClick = (action: Function, column: columnModel) => {
+        try {
+            updateBoardData(action(column))
+        }
+        catch (error) {
+            let message: string = 'Unknown Error :('
+            if (error instanceof Error) {
+                message = error.message
+            }
+            console.error(message)
+            toast({
+                description: message,
+                variant: "destructive",
+                duration: 3000
+            })
+        }
+    }
+
+    const columns: React.ReactNode[] = columnList.map(column =>
+        <Card key={column.id} className="px-6 flex content-center justify-between border">
+            <CardHeader>
+                <CardTitle>
+                    {column.name}
+                </CardTitle>
+            </CardHeader>
+            <CardContent className="py-0 flex items-center">
+                <Button 
+                    onClick={() => handleClick(deleteColumn, column)} 
+                    variant="destructive" className="w-full"
+                > 
+                    <Trash2 size={iconSize} />
+                </Button>
+            </CardContent>
+        </Card>
+    )
+    return (
+        <ul className="h-auto w-full py-5 px-6 flex flex-wrap flex-col justify-start gap-y-3 gap-x-3.5"> 
+            { columns }
+            <li className="self-center sm:self-end"><Button onClick={() => handleClick(addColumn, getNewColumn())} title="Crear columna" ><Plus size={iconSize} /></Button></li>
+        </ul>
+    )
+}
