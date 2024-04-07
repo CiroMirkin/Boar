cd3f0c9a30d5c6eea65f7671aa5827848bd420bf
Ciro Mirkin <ciromirkin@gmail.com>
2024-04-07 21:12:28 +0000

    creado: carpeta board dentro de la carpeta useCases

--- src/useCases/board/changeBoardName.ts
+++ src/useCases/board/changeBoardName.ts
@@ -1,5 +1,5 @@
-import { boardModel } from "../models/board";
-import { changeNameParams } from "./useCase";
+import { boardModel } from "../../models/board";
+import { changeNameParams } from "../useCase";
 
 export function changeBoardName({ board, newName }: changeNameParams): boardModel {
     const newBoard = board
