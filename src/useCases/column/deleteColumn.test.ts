0223b93bf637a2f01e89c8b32ffbd76f6c0c7e29
c.mirkin <ciromirkin@gmail.com>
2024-04-13 10:52:48 -0300

    bug fix: al eliminar columnas no se cambiaba la posicion de las mismas

--- src/useCases/column/deleteColumn.test.ts
+++ src/useCases/column/deleteColumn.test.ts
@@ -4,44 +4,34 @@ import { deleteThisColumn } from "./deleteColumn.ts"
 describe('Eliminar columna.', () => {
     test('Se debería eliminar la columna indicada.', () => {
         const column: columnModel = {
-            id: "c4",
+            id: "c2",
             name: "",
-            position: "4",
+            position: "2",
         }
         const columnList: columnModel[] = [
             {
                 id: "",
-                name: "",
+                name: "pipi",
                 position: "1",
             },
+            {...column},
             {
                 id: "",
-                name: "",
-                position: "2",
-            },
-            {
-                id: "c3",
-                name: "",
+                name: "pupu",
                 position: "3",
             },
-            {...column},
         ]
 
         expect(deleteThisColumn({ columnList, column })).toStrictEqual([
             {
                 id: "",
-                name: "",
+                name: "pipi",
                 position: "1",
             },
             {
                 id: "",
-                name: "",
+                name: "pupu",
                 position: "2",
-            },
-            {
-                id: "c3",
-                name: "",
-                position: "3",
             },
         ])
     })
--- src/useCases/column/deleteColumn.ts
+++ src/useCases/column/deleteColumn.ts
@@ -4,6 +4,11 @@ import BusinessError from "@/errors/businessError";
 
 export function deleteThisColumn({ columnList, column }: columnUseCaseParams): columnModel[] {
     if(columnList.length <= 2) throw new BusinessError('No puedes tener menos de dos columnas.')
-
-    return columnList.filter(columnInBoard => columnInBoard.id !== column.id)
+    const newColumns = columnList
+        .filter(columnInBoard => columnInBoard.id !== column.id)
+        .map((column, index) => {
+            column.position = JSON.stringify(index + 1)
+            return column
+        })
+    return newColumns
 }
