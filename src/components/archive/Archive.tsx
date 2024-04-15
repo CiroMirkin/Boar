babbf439e3850270668d713440693ad1da4c52ac
Ciro Mirkin <107360995+CiroMirkin@users.noreply.github.com>
2024-04-15 13:14:04 +0000

    cambio: se extrajo un componente del componente Archive

--- src/components/archive/Archive.tsx
+++ src/components/archive/Archive.tsx
@@ -37,10 +37,20 @@ export function Archive({ boardArchive}: ArchiveProps) {
             <Header title="Archivo" whereUserIs={USER_IS_IN.ARCHIVE}/>
             <div className="mx-6 my-4 flex flex-col gap-y-2">
                 { archive.length === 0 
-                    ? <Card className="py-2 px-4"><CardHeader><CardTitle>Aún no hay tareas archivadas.</CardTitle></CardHeader></Card> 
+                    ? <EmptyArchive />
                     : archive
                 }
             </div>
         </>
+    )
+}
+
+function EmptyArchive() {
+    return (
+        <Card className="py-2 px-4">
+            <CardHeader>
+                <CardTitle>Aún no hay tareas archivadas.</CardTitle>
+            </CardHeader>
+        </Card>
     )
 }
