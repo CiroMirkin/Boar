075cb162aecf52612689601e9304775b168ae821
Ciro Mirkin <107360995+CiroMirkin@users.noreply.github.com>
2024-04-18 13:48:24 +0000

    agregado: gestion de localstorge para que el modal de bienvenida solo aparezca una vez

--- src/components/board/WelcomeDialog.tsx
+++ src/components/board/WelcomeDialog.tsx
@@ -1,9 +1,15 @@
+import { useEffect } from "react"
 import { Dialog, DialogClose, DialogContent, DialogFooter, DialogHeader, DialogTitle } from "../ui/dialog"
 import { Button } from "@/ui/button"
 
 export function WelcomeDialog(){
+    const open = localStorage.getItem('boar-welcome-dialog') ? JSON.parse(localStorage.getItem('boar-welcome-dialog') as string) : false
+    useEffect(()=> {
+        open == false && localStorage.setItem('boar-welcome-dialog', 'true')
+    }, [])
+
     return(
-        <Dialog defaultOpen>
+        <Dialog defaultOpen={open === false && true}>
             <DialogContent className="sm:max-w-md">
                 <DialogHeader>
                 <DialogTitle>¡Bienvenido!</DialogTitle>
