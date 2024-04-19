e088ab3ba4882a60f7acff158eaad04d386ce657
Ciro Mirkin <107360995+CiroMirkin@users.noreply.github.com>
2024-04-19 12:49:37 +0000

    cambio: texto del dialogo de bienvenida

--- src/components/board/WelcomeDialog.tsx
+++ src/components/board/WelcomeDialog.tsx
@@ -17,7 +17,6 @@ export function WelcomeDialog(){
                 <div className="flex flex-col gap-y-2">
                     <p><span className="font-medium">Boar</span> es un aplicación que busca ayudarte con tus tareas diarias o semanales a través de un tablero.</p>
                     <p>En la primer columna del tablero puedes crear tus tareas y cuando lleguen a la última puedes archivarlas.</p>
-                    <p>Las tareas archivadas se guardan diariamente y puedes verlas en el archivo.</p>
                 </div>
                 <DialogFooter className="sm:justify-start">
                 <DialogClose asChild>
