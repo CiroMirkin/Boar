47247c65c67ffeb0a175734b6bba2388a99674cf
c.mirkin <ciromirkin@gmail.com>
2024-04-22 10:44:34 -0300

    creado: funciones para saber si una lista de tareas esta dentro de su limite

--- src/models/taskListInEachColumn.ts
+++ src/models/taskListInEachColumn.ts
@@ -1,6 +1,20 @@
+import BusinessError from "@/errors/businessError";
 import { taskModel } from "./task";
 
 export type TaskList = taskModel[]
 export type TaskListInEachColumn = TaskList[]
 
 export const emptyTaskListInEachColumn: TaskListInEachColumn = [[], [], []]
+
+const taskListLimit = 14
+const lastTaskListLimit = 30 // Deberia ser igual a la constante dailyArchiveLimit
+
+export const isThisTaskListWithinTheLimit = ({ taskList }: { taskList: TaskList }): true | BusinessError => {
+    if(taskList.length > taskListLimit) throw new BusinessError('La columna esta llena.')
+    return true
+}
+
+export const isThisLastTaskListWithinTheLimit = ({ taskList }: { taskList: TaskList }): true | BusinessError => {
+    if(taskList.length > lastTaskListLimit) throw new BusinessError('La columna esta llena.')
+    return true
+}
