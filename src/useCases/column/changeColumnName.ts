451f4291df510d45db00ac14f547e91afb015fe7
Ciro Mirkin <107360995+CiroMirkin@users.noreply.github.com>
2024-04-25 13:09:54 +0000

    implementado: funcion para validar el nombre de una columna

--- src/useCases/column/changeColumnName.ts
+++ src/useCases/column/changeColumnName.ts
@@ -1,14 +1,12 @@
 import { columnUseCaseParams } from "../useCase";
-import { columnModel, isThisColumnNameWithinTheLimitOfLetters } from "../../models/column";
-import BusinessError from "@/errors/businessError";
+import { columnModel, isThisColumnNameValid } from "../../models/column";
 
 interface changeNameOfColumnParams extends columnUseCaseParams {
     newName: string
 }
 
 export function changeNameOfColumn({ columnList, column, newName }: changeNameOfColumnParams): columnModel[] {
-    if(!newName.trim()) throw new BusinessError('No se pueden crear columnas sin nombre.')
-    if(!isThisColumnNameWithinTheLimitOfLetters(newName)) throw new BusinessError('El nombre es demasiado largo.')
+    isThisColumnNameValid(newName)
     const newColumnList = columnList.map(columnInBoard => {
         if(columnInBoard.id === column.id) {
             columnInBoard.name = newName
