1b3a4a264ef27a9ab4726dc6a8ec631de41d6540
c.mirkin <ciromirkin@gmail.com>
2024-04-26 12:26:03 -0300

    cambio: uso del RouterProvider de React-Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,25 @@
 import './App.css'
 import { Board } from './components/board/Board'
 import { Toaster } from './ui/toaster'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import { Archive } from './components/archive/Archive'
 import { Configs } from './components/configs/Configs'
 import { Erro404 } from './components/404/404'
 import { Help } from './components/help/Help'
 
+const router = createBrowserRouter([
+  { path: "/", element: <Board /> },
+  { path: "/archive", element: <Archive /> },
+  { path: "/settings", element: <Configs /> },
+  { path: "/help", element: <Help /> },
+  { path: "*", element: <Erro404 /> },
+])
+
 function App() {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path='/' 
-            element={
-              <Board />
-            }
-          />
-          <Route path='/archive' 
-            element={
-              <Archive />
-            }
-          />
-          <Route path='/settings' 
-            element={
-              <Configs/>
-            }
-          />
-          <Route path='/help' 
-            element={
-              <Help />
-            }
-          />
-          <Route path='*' element={ <Erro404 /> } />
-        </Routes>
-      </Router>
-      <Toaster/>
+      <RouterProvider router={router} />
+      <Toaster />
     </>
   )
 }
