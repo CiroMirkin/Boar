09371443baa2a5f2a7b7b91877a5d11c8a6b5d22
c.mirkin <ciromirkin@gmail.com>
2024-04-30 17:56:25 -0300

    cambio: funcion downloadArchiveLikePDF para que sorte tareas largas y varios dias de archivo

--- src/utils/downloadArchiveLikePDF.ts
+++ src/utils/downloadArchiveLikePDF.ts
@@ -2,22 +2,41 @@ import { Archive } from "@/models/archive"
 import { jsPDF } from "jspdf";
 
 export const downloadArchiveLikePDF = ({ archive }: { archive: Archive }): void => {
-    const doc = new jsPDF()
+    const doc = new jsPDF('portrait')
     doc.setTextColor('#000000')
-    
-    let Y = 16 // This is the Y Coordinate for texts
-  
+    const INIT_Y = 16 // This is the Y Coordinate for texts
+
+    doc.setFontSize(8)
+    doc.text("Boar", 10, 6)
+   
     archive.forEach(({ date, tasklist }) => {
+      let Y = INIT_Y // This is the Y Coordinate for texts
       doc.setFontSize(26)
       doc.text(date, 10, Y)
-      doc.setFontSize(20)
+      doc.setFontSize(18)
   
       tasklist.forEach(({ descriptionText })=> {
-        Y += 10
-        doc.text(descriptionText, 20, Y)
+        Y += 14
+        let line = 63  // length of the text line
+        let text = descriptionText
+
+        if(text.length >= line) {
+          text = descriptionText.slice(0, line);
+          doc.text(text, 10, Y)
+          Y += 10
+
+          text = descriptionText.slice(line, (line*2)).trimStart();
+          line = (line*2)
+          doc.text(text, 10, Y)
+          Y += 10
+
+          text = descriptionText.slice(line, (line*2)).trimStart();
+          doc.text(text, 10, Y)
+        } else {
+          doc.text(text, 10, Y)
+        }
       })
-  
-      Y += 14
+      doc.addPage('letter', 'portrait')
     })
   
     doc.save('archivo_Boar.pdf')
