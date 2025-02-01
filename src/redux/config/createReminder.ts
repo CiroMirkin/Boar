e3ca408d7fffbd964692ce1b1fc19291c02968da
Ciro Mirkin <107360995+CiroMirkin@users.noreply.github.com>
2025-02-01 01:43:55 +0000

    arreglado: validacion de los datos al crear un recordatorio

--- src/redux/config/createReminder.ts
+++ src/redux/config/createReminder.ts
@@ -3,7 +3,7 @@ import { Reminder } from "@/models/reminder"
 
 export const createReminder = (newReminder: Reminder) => {
     if(!newReminder.text.trim()) throw new BusinessError('El recordatorio debe tener una descripción.')
-    if(!!newReminder.columnPosition) throw new BusinessError('El recordatorio debe efectuarse sobre una columna.')
+    if(!newReminder.columnPosition) throw new BusinessError('El recordatorio debe efectuarse sobre una columna.')
 
     return newReminder
 }
