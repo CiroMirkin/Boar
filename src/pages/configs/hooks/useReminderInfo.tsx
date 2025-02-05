785448f17618cf5ad4a8f5a63d5050076f265f03
Ciro Mirkin <107360995+CiroMirkin@users.noreply.github.com>
2025-02-05 02:10:06 +0000

    eliminado: copia de la carpeta configs

--- src/pages/configs/hooks/useReminderInfo.tsx
+++
@@ -1,8 +0,0 @@
-import { Reminder } from "@/models/reminder";
-import { RootState } from "@/redux/store";
-import { useSelector } from "react-redux";
-
-/** Proporciona toda la información del recordatorio creado por el usuario. */
-export const useReminderInfo = (): Reminder => {
-    return useSelector((state: RootState) => state.config.reminder)
-}
