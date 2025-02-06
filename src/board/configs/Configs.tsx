ce089cfdf3b11e78a96f8c1a064c5ab3890b3507
Ciro Mirkin <107360995+CiroMirkin@users.noreply.github.com>
2025-02-06 01:22:32 +0000

    cambio: aplicacion de tell dont ask a Reminder

--- src/board/configs/Configs.tsx
+++ src/board/configs/Configs.tsx
@@ -3,12 +3,9 @@ import { USER_IS_IN } from '../../components/userIsIn'
 import { ConfigColumns } from './components/ConfigColumns'
 import { Separator } from '@/ui/separator'
 import { ChangeBoardName } from './components/ChangeBoardName'
-import { useColumnList } from '@/columnList/hooks/useColumnList'
 import Reminder from './components/Reminder'
 
 export function Configs() {
-	const columnList = useColumnList()
-
 	return (
 		<>
 			<Header title='Ajustes' whereUserIs={USER_IS_IN.CONFIG} />
@@ -21,9 +18,7 @@ export function Configs() {
 			</div>
 			<Separator />
 			<div className='py-4 md:px-11 px-6'>
-				<h2 className='text-2xl'>Crear recordatorio</h2>
-				<p className="opacity-75">Cada vez que una tarea llegué a la columna indicada se mostrara el recordatorio en pantalla.</p>
-				<Reminder columnList={columnList} />
+				<Reminder />
 			</div>
 			<Separator />
 		</>
--- src/board/configs/components/Reminder.tsx
+++ src/board/configs/components/Reminder.tsx
@@ -9,19 +9,16 @@ import {
     SelectTrigger,
     SelectValue,
 } from "@/ui/select"
-import { ColumnList } from "@/columnList/models/columnList"
 import { Reminder as reminder } from "../models/reminder"
 import { useToast } from "@/ui/use-toast"
 import { useDispatch } from "react-redux"
 import { addReminder } from "@/board/configs/state/configReducer"
 import getErrorMessageForTheUser from "@/utils/getErrorMessageForTheUser"
 import { useReminderInfo } from "@/board/configs/hooks/useReminderInfo"
+import { useColumnList } from "@/columnList/hooks/useColumnList"
 
-interface ReminderProps {
-    columnList: ColumnList
-}
-
-function Reminder({ columnList }: ReminderProps) {
+function Reminder({ }) {
+    const columnList = useColumnList()
     const reminder = useReminderInfo()
     const [reminderText, setReminderText] = useState(reminder.text as string)
     const [reminderColumnPosition, setReminderColumnPosition] = useState(reminder.columnPosition as string)
@@ -56,38 +53,42 @@ function Reminder({ columnList }: ReminderProps) {
     }
 
     return (
-        <div className="max-w-2xl py-5 grid gap-3">
-            <div className='grid mr-2 w-full items-center gap-1.5'>
-                <Label>Selecciona una columna</Label>
-                <Select value={reminderColumnPosition} onValueChange={(value) => setReminderColumnPosition(value)}>
-                    <SelectTrigger className="w-full">
-                        <SelectValue placeholder="Columnas" />
-                    </SelectTrigger>
-                    <SelectContent>
-                        { columnList.map(column => 
-                            <SelectItem value={column.position} key={column.id} >
-                                { column.name }
-                            </SelectItem>
-                        ) }
-                    </SelectContent>
-                </Select>
-            </div>
+        <>
+            <h2 className='text-2xl'>Crear recordatorio</h2>
+			<p className="opacity-75">Cada vez que una tarea llegué a la columna indicada se mostrara el recordatorio en pantalla.</p>
+            <div className="max-w-2xl py-5 grid gap-3">
+                <div className='grid mr-2 w-full items-center gap-1.5'>
+                    <Label>Selecciona una columna</Label>
+                    <Select value={reminderColumnPosition} onValueChange={(value) => setReminderColumnPosition(value)}>
+                        <SelectTrigger className="w-full">
+                            <SelectValue placeholder="Columnas" />
+                        </SelectTrigger>
+                        <SelectContent>
+                            { columnList.map(column => 
+                                <SelectItem value={column.position} key={column.id} >
+                                    { column.name }
+                                </SelectItem>
+                            ) }
+                        </SelectContent>
+                    </Select>
+                </div>
 
-            <div className='grid mr-2 w-full items-center gap-1.5'>
-                <Label htmlFor='reminder-text'>Escribe la descripción</Label>
-                <Input
-                    type='text'
-                    id='reminder-text'
-                    value={reminderText}
-                    onChange={handleChange}
-                    placeholder='No olvides...'
-                />
-            </div>
+                <div className='grid mr-2 w-full items-center gap-1.5'>
+                    <Label htmlFor='reminder-text'>Escribe la descripción</Label>
+                    <Input
+                        type='text'
+                        id='reminder-text'
+                        value={reminderText}
+                        onChange={handleChange}
+                        placeholder='No olvides...'
+                    />
+                </div>
 
-            <Button onClick={handleClick} variant='ghost'>
-                Crear
-            </Button>
-        </div>
+                <Button onClick={handleClick} variant='ghost'>
+                    Crear
+                </Button>
+            </div>
+        </>
     )
 }
 
