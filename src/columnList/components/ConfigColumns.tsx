8c536846baf2e4121c66fb786e395c96124b84a6
Ciro Mirkin <107360995+CiroMirkin@users.noreply.github.com>
2025-02-10 14:07:08 +0000

    implementado: i18next en el componente ConfigColumns

--- src/columnList/components/ConfigColumns.tsx
+++ src/columnList/components/ConfigColumns.tsx
@@ -4,10 +4,12 @@ import { AddNewColumnForm } from './AddNewColumnForm'
 import LocalStorageColumnListRepository from '@/columnList/state/localStorageColumnList'
 import { ColumnListRepository } from '@/columnList/state/columnListRepository'
 import { useColumnList } from '@/columnList/hooks/useColumnList'
+import { useTranslation } from 'react-i18next'
 
 const columnListRepository: ColumnListRepository = new LocalStorageColumnListRepository()
 
 export function ConfigColumns({ }) {
+	const { t } = useTranslation()
 	const columnList = useColumnList()
 	useEffect(() => columnListRepository.save(columnList), [columnList])
 
@@ -16,7 +18,7 @@ export function ConfigColumns({ }) {
 	))
 	return (
 		<>
-			<h2 className='text-2xl'>Columnas</h2>
+			<h2 className='text-2xl'>{ t('settings.columns.section_title') }</h2>
 			<ul className='h-auto w-full max-w-2xl py-5 flex flex-wrap flex-col justify-start gap-y-3 gap-x-3.5'>
 				{columns}
 				<AddNewColumnForm />
