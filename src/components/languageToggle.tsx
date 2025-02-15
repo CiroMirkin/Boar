96dd4fac04679810ff33f28aefde6f5739552cf6
Ciro Mirkin <107360995+CiroMirkin@users.noreply.github.com>
2025-02-15 15:30:22 +0000

    agregado: componente LanguageToggle extraido del componente Header

---
+++ src/components/languageToggle.tsx
@@ -0,0 +1,51 @@
+import {
+	DropdownMenuRadioGroup,
+	DropdownMenuRadioItem,
+	DropdownMenuPortal,
+	DropdownMenuSub,
+	DropdownMenuSubContent,
+	DropdownMenuSubTrigger,
+} from '@/ui/dropdown-menu'
+import { Languages } from 'lucide-react'
+import { iconSize } from '@/configs/iconsConstants'
+import { useTranslation } from 'react-i18next'
+import i18next from 'i18next'
+import { useLocalStorage, usePreferredLanguage  } from "@uidotdev/usehooks"
+
+export function LanguageToggle() {
+    const [language, setLanguage] = useLocalStorage("language", "es")
+	const { t, i18n } = useTranslation()
+
+	if(i18n.language !== language) {
+		i18next.changeLanguage(language)
+		document.body.dir = i18n.dir()
+	} 
+	else {
+		const userPrerredLanguage = usePreferredLanguage().slice(0, 2)
+		i18next.changeLanguage(userPrerredLanguage === "es" ? "es" : "en")
+		setLanguage(userPrerredLanguage === "es" ? "es" : "en")
+		document.body.dir = i18n.dir()
+	}
+	
+	const handleValueChange = (value: string) => {
+		setLanguage(value)
+		i18next.changeLanguage(value)
+		document.body.dir = i18n.dir()
+	}
+
+    return (
+        <DropdownMenuSub>
+			<DropdownMenuSubTrigger>
+				<Languages size={iconSize} className='mr-2' /> { t('menu.language') }
+			</DropdownMenuSubTrigger>
+			<DropdownMenuPortal>
+				<DropdownMenuSubContent>
+					<DropdownMenuRadioGroup value={language} onValueChange={handleValueChange}>
+						<DropdownMenuRadioItem value="es">Español</DropdownMenuRadioItem>
+						<DropdownMenuRadioItem value="en">English</DropdownMenuRadioItem>
+					</DropdownMenuRadioGroup>
+				</DropdownMenuSubContent>
+			</DropdownMenuPortal>
+		</DropdownMenuSub>
+    )
+}
