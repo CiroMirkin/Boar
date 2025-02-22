360482a05cc3b9ec2f3c95fd4a4de801d5ca0d0e
Ciro Mirkin <107360995+CiroMirkin@users.noreply.github.com>
2025-02-22 05:51:28 +0000

    eliminado: codigo muerto

--- src/modules/board/state/boardReducer.ts
+++ src/modules/board/state/boardReducer.ts
@@ -3,15 +3,12 @@ import { BoardRepository } from '@/modules/board/models/boardRepository'
 import LocalStorageBoardRepository from '@/modules/board/state/localstorageBoard'
 import { changeBoardName } from '@/modules/board/state/actions/changeBoardName'
 import { PayloadAction, createSlice } from '@reduxjs/toolkit'
-import { ColorTheme, defaultColorTheme } from '@/modules/shared/configs/colors'
 
 interface InitialState {
 	board: boardModel
-	colorTheme: ColorTheme
 }
 const boardRepository: BoardRepository = new LocalStorageBoardRepository()
 const initialState: InitialState = {
-	colorTheme: { ...defaultColorTheme },
 	board: boardRepository.getAll(),
 }
 
@@ -23,15 +20,8 @@ export const boardSlice = createSlice({
 			const newName = action.payload
 			state.board = changeBoardName({ board: state.board, newName })
 		},
-		setColorTheme: (state, action: PayloadAction<ColorTheme>) => {
-			const { bg, text, task, column } = action.payload
-			state.colorTheme.bg = bg
-			state.colorTheme.text = text
-			state.colorTheme.task = task
-			state.colorTheme.column = column
-		},
 	},
 })
 
-export const { changeTheNameOfTheBoard, setColorTheme } = boardSlice.actions
+export const { changeTheNameOfTheBoard } = boardSlice.actions
 export default boardSlice.reducer
