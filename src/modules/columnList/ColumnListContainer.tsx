bf3ca747905c4713f60b5ef4bb2086fd5ad0c7f0
Ciro Mirkin <107360995+CiroMirkin@users.noreply.github.com>
2025-02-22 08:38:42 +0000

    eliminado: codigo muerto

--- src/modules/columnList/ColumnListContainer.tsx
+++ src/modules/columnList/ColumnListContainer.tsx
@@ -1,15 +1,12 @@
-import { TaskListInEachColumnProvider } from './taskList/contexts/TaskListInEachColumnContext'
 import { ColumnList, ColumnsContent } from './ColumnList'
 import { ColumnsContextContent, ColumnsProvider } from './ColumnsContext'
 
 function ColumnListContainer({ children, columnsData }: { children: () => ColumnsContent, columnsData: ColumnsContextContent}) {
 	return (
 		<ColumnsProvider value={columnsData}>
-		<TaskListInEachColumnProvider>
 			<div className='h-auto pb-5 px-6 md:px-11 flex flex-wrap justify-stretch gap-y-3 gap-x-4'>
 				<ColumnList>{ children }</ColumnList>
 			</div>
-		</TaskListInEachColumnProvider>
 		</ColumnsProvider>
 	)
 }
