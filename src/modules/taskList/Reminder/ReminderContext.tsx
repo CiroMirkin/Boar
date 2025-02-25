b9dd6277be5c9e3156ba7d06fdafc79359c37b57
Ciro Mirkin <107360995+CiroMirkin@users.noreply.github.com>
2025-02-25 20:43:01 +0000

    agregado: ReminderContext

---
+++ src/modules/taskList/Reminder/ReminderContext.tsx
@@ -0,0 +1,29 @@
+import { createContext, Dispatch, SetStateAction } from "react";
+import { blankReminder, Reminder } from "./reminder";
+
+
+interface ReminderContextData {
+    reminder: Reminder
+    setReminder: Dispatch<SetStateAction<Reminder>>
+}
+
+const defaultReminderContextValue: ReminderContextData = {
+    reminder: blankReminder,
+    setReminder: () => {}
+}
+
+const ReminderContext = createContext(defaultReminderContextValue)
+
+interface ReminderProviderProps {
+    children: React.ReactNode
+    reminderData: ReminderContextData
+}
+
+export function ReminderProvider({ children, reminderData }: ReminderProviderProps){
+    return (
+        <ReminderContext.Provider value={reminderData}>
+            { children }
+        </ReminderContext.Provider>
+    )
+}
+
