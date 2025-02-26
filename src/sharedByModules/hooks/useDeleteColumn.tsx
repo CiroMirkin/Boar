e6918902c9d983359bae070bff497fa19423b954
Ciro Mirkin <107360995+CiroMirkin@users.noreply.github.com>
2025-02-26 13:07:28 +0000

    agregado: useDeleteColumn

---
+++ src/sharedByModules/hooks/useDeleteColumn.tsx
@@ -0,0 +1,12 @@
+import { Column } from "@/modules/columnList/models/column"
+import { useDispatch } from 'react-redux'
+import { deleteTheTaskListOfThisColumn } from '@/modules/taskList/state/taskListInEachColumnReducer'
+import { deleteColumn } from '@/modules/columnList/state/columnListReducer'
+
+export const useDeleteColumn = () => {
+	const updateBoardData = useDispatch()
+    return ({ column }: { column: Column }) => {
+        updateBoardData(deleteColumn(column))
+		updateBoardData(deleteTheTaskListOfThisColumn(column))
+    }
+}
