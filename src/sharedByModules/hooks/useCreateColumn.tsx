90970fcdf593ac25b9b74d6343e8eb6985db1cc1
Ciro Mirkin <107360995+CiroMirkin@users.noreply.github.com>
2025-02-26 13:00:03 +0000

    agregado: useCreateColumn

---
+++ src/sharedByModules/hooks/useCreateColumn.tsx
@@ -0,0 +1,12 @@
+import { useDispatch } from 'react-redux'
+import { addColumn } from '@/modules/columnList/state/columnListReducer'
+import { addEmptyTaskListAtTheEnd } from '@/modules/taskList/state/taskListInEachColumnReducer'
+import { Column } from '@/modules/columnList/models/column'
+
+export const useCreateColumn = () => {
+	const updateBoardData = useDispatch()
+    return ({ newColumn }: { newColumn: Column }) => {
+        updateBoardData(addColumn(newColumn))
+		updateBoardData(addEmptyTaskListAtTheEnd())
+    }
+}
