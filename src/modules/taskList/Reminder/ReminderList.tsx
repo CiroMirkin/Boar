bffe1fb919845ae656966da95d85bb9a22394c97
Ciro Mirkin <107360995+CiroMirkin@users.noreply.github.com>
2025-02-27 21:25:22 +0000

    agregado: internacionalizacion del componente ReminderList

--- src/modules/taskList/Reminder/ReminderList.tsx
+++ src/modules/taskList/Reminder/ReminderList.tsx
@@ -5,11 +5,13 @@ import { Trash } from "lucide-react"
 import { iconSize } from "@/sharedByModules/configs/iconsConstants"
 import { blankReminder } from "./reminder"
 import { useAskForConfirmationToast } from "@/sharedByModules/hooks/useAskForConfirmationToast"
+import { useTranslation } from "react-i18next"
 
 export function ReminderList(){
+    const { t } = useTranslation()
     return (
         <div>
-            <h2 className='text-2xl mb-4'>Recordatorios creados</h2>
+            <h2 className='text-2xl mb-4'>{ t('settings.reminder.reminder_list_section_title') }</h2>
             <ul>
                 { getReminderList() }
             </ul>
@@ -18,6 +20,7 @@ export function ReminderList(){
 } 
 
 const getReminderList = () => {
+    const t = useTranslation().t
     const { reminder, setReminder } = useContext(ReminderContext)
 
     const deleteReminder = () => {
@@ -25,7 +28,7 @@ const getReminderList = () => {
     }
 
     const askForConfirmationToDeleteTheReminder = useAskForConfirmationToast({
-        confirmationText: '¿Seguro desea eliminar este recordatorio?',
+        confirmationText: t('settings.reminder.delete_reminder_warning_toast'),
         action: deleteReminder
     })
 
@@ -33,7 +36,7 @@ const getReminderList = () => {
         <li className='w-full py-1 px-3 flex flex-col gap-2 content-stretch border rounded-md'>
             <div className="w-full flex justify-between items-center gap-2">
                 <p className="text-xl">{ reminder.text }</p>
-                <Button variant='destructiveGhost' title='Eliminar recordatorio' onClick={askForConfirmationToDeleteTheReminder}>
+                <Button variant='destructiveGhost' title={t('settings.reminder.delete_reminder_btn')} onClick={askForConfirmationToDeleteTheReminder}>
                 <Trash size={iconSize} />
                 </Button>
             </div>
@@ -42,7 +45,7 @@ const getReminderList = () => {
 
     const blankReminderList = (
         <p className="w-full py-1 px-3 opacity-75 border rounded-md">
-            Aún no haz creado ningun recordatorio.
+            {t('settings.reminder.blank_reminder_list')}
         </p>
     )
 
