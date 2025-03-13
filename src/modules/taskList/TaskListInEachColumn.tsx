2bd9059fd278fea8a334c8e7b64bbc6a0dd9da79
Ciro Mirkin <107360995+CiroMirkin@users.noreply.github.com>
2025-03-13 14:18:28 +0000

    agregado: implementacion de LocalStorageTaskListInEachColumnRepository

--- src/modules/taskList/TaskListInEachColumn.tsx
+++ src/modules/taskList/TaskListInEachColumn.tsx
@@ -2,9 +2,11 @@ import { TaskList } from './TaskList'
 import { ScrollArea } from '@/ui/scroll-area'
 import { useReminder } from '@/modules/taskList/Reminder/useReminder'
 import { useTaskListInEachColumn } from './hooks/useTaskListInEachColumn'
+import LocalStorageTaskListInEachColumnRepository from './state/localStorageTaskLists'
 
 export function TaskListInEachColumn() {
     const taskListInEachColumn = useTaskListInEachColumn()
+	new LocalStorageTaskListInEachColumnRepository().save(taskListInEachColumn)
 	useReminder(taskListInEachColumn)
 
 	const columnsContent: React.ReactNode[] = []
