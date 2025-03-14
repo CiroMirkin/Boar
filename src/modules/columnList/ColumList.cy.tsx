6ef466be24c92985177aa73efbd9348748681587
c.mirkin <ciromirkin@gmail.com>
2025-03-14 19:00:20 -0300

    agregado: prueba que valida el movimiento de las tareas

--- src/modules/columnList/ColumList.cy.tsx
+++ src/modules/columnList/ColumList.cy.tsx
@@ -24,4 +24,22 @@ describe("Lista de columnas y tareas.", () => {
         cy.get("[id=add_new_task_btn]").click()
         cy.get(".taskList").children().first().contains(textOfTask)
     })
+    it("El usuario puede mover la tarea creada hasta la ultima columna.", () => {
+        const columnsData: ColumnsContextContent = {
+            firstColumnFooterContent: <AddNewTaskInput/>,
+            lastColumnFooterContent: <ArchiveTaskListButton/>,
+        }
+        cy.mount(
+            <Provider store={store}>
+                <ColumnListContainer columnsData={columnsData}>
+                    { TaskListInEachColumn }
+                </ColumnListContainer>
+            </Provider>
+        )
+        // Busca y hace click en el botón para avanzar la primer tarea de la primer columna
+        cy.get(".taskList").eq(0).children().first().click().children().last().children().children().first().next().click()
+        cy.get(".taskList").eq(1).children().first().click().children().last().children().children().first().next().click()
+        // Se busca la segunda columna y la tarea movida
+        cy.get(".taskList").eq(2).children().first().contains(textOfTask)
+    })
 })
