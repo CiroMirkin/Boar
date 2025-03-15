88e80e1c278e5d1cd4d7b8059690652322db83bd
Ciro Mirkin <107360995+CiroMirkin@users.noreply.github.com>
2025-03-15 19:16:50 +0000

    cambio: uso de useTheme en el modulo Board

--- src/modules/board/Board.tsx
+++ src/modules/board/Board.tsx
@@ -2,15 +2,13 @@ import { Header } from '../../sharedByModules/Header/Header'
 import { USER_IS_IN } from '../../sharedByModules/Header/userIsIn'
 import { WelcomeDialog } from './components/WelcomeDialog'
 import { useBoard } from '@/modules/board/hooks/useBoard'
-import { useContext } from 'react'
-import { ThemeContext } from '../../sharedByModules/Theme/ThemeContext'
+import { useTheme } from '../../sharedByModules/Theme/ThemeContext'
 import { useDocumentTitle } from '@uidotdev/usehooks'
 
 export function Board({ children }: { children: React.ReactNode }) {
 	const data = useBoard()
 	useDocumentTitle(data.name + " - Boar")
-	const { theme } = useContext(ThemeContext)
-	const { bg, text } = theme
+	const { bg, text } = useTheme()
 
 	return (
 		<div className={`xl:h-screen sm:h-full ${bg} ${text}`}>
