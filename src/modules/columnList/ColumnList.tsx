68609466ac3472a26eddb36d90c1ee594242b056
CiroMirkin <ciromirkin@gmail.com>
2025-05-20 23:07:25 -0300

    agregado: guardado de cambios en las columnas dentro de supabase

--- src/modules/columnList/ColumnList.tsx
+++ src/modules/columnList/ColumnList.tsx
@@ -1,5 +1,8 @@
+import { useSession } from '@/SessionProvider'
 import { Column } from './components/Column'
 import { useColumnList } from './hooks/useColumnList'
+import { useEffect } from 'react'
+import { sendForSaveColumnList } from './state/sendForSaveColumnList'
 
 export type ColumnsContent = React.ReactNode[]
 
@@ -9,6 +12,11 @@ interface ColumnListProps {
 
 export function ColumnList({ children: getColumnsContent }: ColumnListProps) {
 	const columns = useColumnList()
+
+	const { session } = useSession()
+	useEffect(() => {
+		if(!!session) sendForSaveColumnList(columns)
+	}, [])
 	const columnsContent = getColumnsContent()
 
 	const columnList = columns.map((column, columnIndex) => {
