8a50dca5d70a3249685c13a98a0a0475b89edb17
CiroMirkin <ciromirkin@gmail.com>
2025-05-29 17:16:47 -0300

    agregado: custom hook que permite gestionar la sincronizacion de la entidad Board

---
+++ src/modules/board/state/useSaveBoard.tsx
@@ -0,0 +1,25 @@
+import { SessionType } from "@/SessionProvider"
+import { boardModel, defaultBoard } from "../models/board"
+import LocalStorageBoardRepository from "./localstorageBoard"
+import { sendForSaveBoard } from "./sendForSaveBoard"
+
+interface useSaveBoardParams {
+    data: boardModel
+    session: SessionType
+}
+
+export const useSaveBoard = ({ data, session }: useSaveBoardParams) => {
+    const localStorage = new LocalStorageBoardRepository()
+    const localBoard = localStorage.getAll()
+
+    const isNotTheLocalBoard = JSON.stringify(data) !== JSON.stringify(localBoard)
+    const isNotTheDefaultBoard = JSON.stringify(data) !== JSON.stringify(defaultBoard)
+    if(isNotTheDefaultBoard) {
+        if(!!session && isNotTheLocalBoard) {
+            sendForSaveBoard(data)
+        }
+        else {
+            localStorage.save(data)
+        } 
+    }
+}
