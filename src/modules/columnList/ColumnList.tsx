5fc3521bafaeeddb2567063961cbb777d1af103e
CiroMirkin <ciromirkin@gmail.com>
2025-05-29 17:45:48 -0300

    agregado: custom hook que permite gestionar la sincronizacion de la entidad ColumnList

--- src/modules/columnList/ColumnList.tsx
+++ src/modules/columnList/ColumnList.tsx
@@ -2,7 +2,7 @@ import { useSession } from '@/SessionProvider'
 import { Column } from './components/Column'
 import { useColumnList } from './hooks/useColumnList'
 import { useEffect } from 'react'
-import { sendForSaveColumnList } from './state/sendForSaveColumnList'
+import { useSaveColumnList } from './state/useSaveColumnList'
 
 export type ColumnsContent = React.ReactNode[]
 
@@ -15,8 +15,8 @@ export function ColumnList({ children: getColumnsContent }: ColumnListProps) {
 
 	const { session } = useSession()
 	useEffect(() => {
-		if(!!session) sendForSaveColumnList(columns)
-	}, [])
+		useSaveColumnList({ session, columnList: columns })
+	}, [columns])
 	const columnsContent = getColumnsContent()
 
 	const columnList = columns.map((column, columnIndex) => {
