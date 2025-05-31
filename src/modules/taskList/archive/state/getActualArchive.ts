429dca091f365b08cbc75707cd84ff775088dbc7
CiroMirkin <ciromirkin@gmail.com>
2025-05-30 21:47:56 -0300

    agregado: comentario a la funcion getActualArchive

--- src/modules/taskList/archive/state/getActualArchive.ts
+++ src/modules/taskList/archive/state/getActualArchive.ts
@@ -1,4 +1,5 @@
 import { store } from "@/store";
 import { Archive } from "../models/archive";
 
+/** Permite recuperar el archivo sin usar el hook useSelector() */
 export const getActalArchive = (): Archive => store.getState().archive.list 
