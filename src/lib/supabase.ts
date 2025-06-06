784152361feb273ff05b7296445b91c8a1c802a4
CiroMirkin <ciromirkin@gmail.com>
2025-06-05 22:16:54 -0300

    cambio: nombre de las variables de entorno

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js"
 
-const supabaseUrl: string = process.env.SUPABASE_URL || ""
-const supabaseKey: string = process.env.SUPABASE_ANON_KEY || ""
+const supabaseUrl: string = process.env.REACT_APP_SUPABASE_URL || ""
+const supabaseKey: string = process.env.REACT_APP_SUPABASE_ANON_KEY || ""
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
