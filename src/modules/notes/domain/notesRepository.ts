762333e436a72b936d1ca235b940337b7cf055cf
CiroMirkin <ciromirkin@gmail.com>
2025-06-21 14:42:38 -0300

    agregado: localStorage repository para las notas

---
+++ src/modules/notes/domain/notesRepository.ts
@@ -0,0 +1,6 @@
+import { Notes } from "./notes"
+
+export interface NotesRepository {
+    save(notes: Notes): void
+    getAll(): Notes
+}
---
+++ src/modules/notes/repository/LocalStorageNotesRepository.ts
@@ -0,0 +1,19 @@
+import { Notes } from "../domain/notes"
+import { NotesRepository } from "../domain/notesRepository"
+
+export default class LocalStorageNotesRepository implements NotesRepository {
+    key
+    constructor() {
+        this.key = 'boar-notes'
+    }
+    save(notes: Notes): void {
+        const notesForSave = { notes }
+        localStorage.setItem(this.key, JSON.stringify(notesForSave))
+    }
+    getAll(): Notes {
+        const defaultNotes = ''
+        return localStorage.getItem(this.key)
+            ? JSON.parse(localStorage.getItem(this.key) as string).notes
+            : defaultNotes
+    }
+}
