c9a13f2392342e886a6d682c8d8d709430de4b9e
CiroMirkin <ciromirkin@gmail.com>
2025-06-20 21:25:21 -0300

    agregado: implementacion del modulo notes

--- src/sharedByModules/Header/Header.tsx
+++ src/sharedByModules/Header/Header.tsx
@@ -15,6 +15,7 @@ import { LanguageToggle } from './LanguageToggle'
 import { useTranslation } from 'react-i18next'
 import LogInAndLogOutMenuItem from './LogInAndLogOutMenuItem'
 import { useSession } from '@/SessionProvider'
+import Notes from '@/modules/notes/Notes'
 
 interface HeaderProps {
 	title: string
@@ -27,49 +28,52 @@ export function Header({ title, whereUserIs }: HeaderProps) {
 	return (
 		<header className='w-full px-6 md:px-11 py-7 flex justify-between items-start'>
 			<h1 className='text-4xl font-normal'>{title}</h1>
-			<DropdownMenu>
-				<DropdownMenuTrigger asChild>
-					<Button variant='outline' className='text-black'>
-						<Menu size={iconSize} />
-					</Button>
-				</DropdownMenuTrigger>
-				<DropdownMenuContent>
-					<DropdownMenuLabel>Boar</DropdownMenuLabel>
-					<DropdownMenuSeparator />
-					<DropdownMenuItem disabled={whereUserIs === USER_IS_IN.BOARD && true}>
-						<Link to='/' className='flex items-center'>
-							<Columns3 size={iconSize} className='mr-2' /> {t('menu.board')}
-						</Link>
-					</DropdownMenuItem>
-					<DropdownMenuItem disabled={whereUserIs === USER_IS_IN.ARCHIVE && true}>
-						<Link to='/archive' className='flex items-center'>
-							<Archive size={iconSize} className='mr-2' /> {t('menu.archive')}
-						</Link>
-					</DropdownMenuItem>
-					<DropdownMenuItem disabled={whereUserIs === USER_IS_IN.CONFIG && true}>
-						<Link to='/settings' className='flex items-center'>
-							<Settings size={iconSize} className='mr-2' /> {t('menu.configs')}
-						</Link>
-					</DropdownMenuItem>
-					<DropdownMenuSeparator />
-					<LanguageToggle />
-					<DropdownMenuItem disabled={whereUserIs === USER_IS_IN.HELP && true}>
-						<Link to='/help' className='flex items-center'>
-							<CircleHelp size={iconSize} className='mr-2' /> {t('menu.help')}
-						</Link>
-					</DropdownMenuItem>
-					<DropdownMenuItem>
-						<a href='https://github.com/CiroMirkin/Boar' className='flex items-center'>
-							<Github size={iconSize} className='mr-2' /> GitHub
-						</a>
-					</DropdownMenuItem>
-					<DropdownMenuSeparator />
-					<LogInAndLogOutMenuItem 
-						whereUserIs={whereUserIs} 
-						session={session}
-					/>
-				</DropdownMenuContent>
-			</DropdownMenu>
+			<div className='flex gap-2 items-center'>
+				<Notes/>
+				<DropdownMenu>
+					<DropdownMenuTrigger asChild>
+						<Button variant='outline' className='text-black'>
+							<Menu size={iconSize} />
+						</Button>
+					</DropdownMenuTrigger>
+					<DropdownMenuContent>
+						<DropdownMenuLabel>Boar</DropdownMenuLabel>
+						<DropdownMenuSeparator />
+						<DropdownMenuItem disabled={whereUserIs === USER_IS_IN.BOARD && true}>
+							<Link to='/' className='flex items-center'>
+								<Columns3 size={iconSize} className='mr-2' /> {t('menu.board')}
+							</Link>
+						</DropdownMenuItem>
+						<DropdownMenuItem disabled={whereUserIs === USER_IS_IN.ARCHIVE && true}>
+							<Link to='/archive' className='flex items-center'>
+								<Archive size={iconSize} className='mr-2' /> {t('menu.archive')}
+							</Link>
+						</DropdownMenuItem>
+						<DropdownMenuItem disabled={whereUserIs === USER_IS_IN.CONFIG && true}>
+							<Link to='/settings' className='flex items-center'>
+								<Settings size={iconSize} className='mr-2' /> {t('menu.configs')}
+							</Link>
+						</DropdownMenuItem>
+						<DropdownMenuSeparator />
+						<LanguageToggle />
+						<DropdownMenuItem disabled={whereUserIs === USER_IS_IN.HELP && true}>
+							<Link to='/help' className='flex items-center'>
+								<CircleHelp size={iconSize} className='mr-2' /> {t('menu.help')}
+							</Link>
+						</DropdownMenuItem>
+						<DropdownMenuItem>
+							<a href='https://github.com/CiroMirkin/Boar' className='flex items-center'>
+								<Github size={iconSize} className='mr-2' /> GitHub
+							</a>
+						</DropdownMenuItem>
+						<DropdownMenuSeparator />
+						<LogInAndLogOutMenuItem 
+							whereUserIs={whereUserIs} 
+							session={session}
+						/>
+					</DropdownMenuContent>
+				</DropdownMenu>
+			</div>
 		</header>
 	)
 }
