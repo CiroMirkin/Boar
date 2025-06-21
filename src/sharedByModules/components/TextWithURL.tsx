766e4c781055456b6e03243b0272ea3d0abedc2b
CiroMirkin <ciromirkin@gmail.com>
2025-06-21 20:37:03 -0300

    agregado: funcion para dar formato al texto de los enlaces en las tareas

--- src/sharedByModules/components/TextWithURL.tsx
+++ src/sharedByModules/components/TextWithURL.tsx
@@ -11,27 +11,26 @@ export function TextWithURL({ text }: TextWithURLProps) {
 		const url = splitText[0]
 		return (
 			<>
-				Enlace:{' '}
 				<a href={url} target='_blank' className='text-blue-600 hover:underline'>
-					{url}
+					{ addSpacesAfterSlashes(url) }
 				</a>
 			</>
 		)
 	}
 
-	const textWithURL: React.ReactNode = splitText.map((t, tIndex) => {
+	const textWithURL: React.ReactNode = splitText.map((text, tIndex) => {
 		// Se buscan y convierten todos los enlaces en el texto recibido.
-		if (isValidURL(t)) {
+		if (isValidURL(text)) {
 			return (
-				<span key={t + tIndex}>
+				<span key={text + tIndex}>
 					{' '}
-					<a href={t} target='_blank' className='text-blue-600 hover:underline'>
-						{t}
+					<a href={text} target='_blank' className='text-blue-600 hover:underline'>
+						{ addSpacesAfterSlashes(text) }
 					</a>{' '}
 				</span>
 			)
 		}
-		return <span key={t + tIndex}>{t} </span>
+		return <span key={text + tIndex}>{text} </span>
 	})
 
 	return <>{textWithURL}</>
@@ -54,4 +53,19 @@ const isValidURL = (urlString: string): boolean => {
 		'i'
 	)
 	return !!patronURL.test(urlString)
+}
+
+function addSpacesAfterSlashes(url: string): string {
+  const [protocol, ...rest] = url.split('://')
+  const remainingUrl = rest.join('://')
+  
+  // Agregar espacios despues de cada / despues del protocolo
+  let urlWithSpaces = remainingUrl.replace(/\//g, '/ ')
+  
+  // Agregar espacios en cadenas largas de letras/numeros (mas de 12 caracteres)
+  urlWithSpaces = urlWithSpaces.replace(/[a-zA-Z0-9]{13,}/g, (match) => {
+    return match.replace(/(.{12})/g, '$1 ').trim()
+  })
+  
+  return `${protocol}://${urlWithSpaces}`
 }
