3648d951495d3545dfd21c3142063edab48c95f3
CiroMirkin <ciromirkin@gmail.com>
2025-06-22 16:03:53 -0300

    agregado: eventos de drag and drop

--- src/modules/columnList/components/Column.tsx
+++ src/modules/columnList/components/Column.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useContext } from 'react'
+import React, { createContext, DragEvent, useContext, useState } from 'react'
 import { Column as columnModel, columnNull } from '../models/column'
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/ui/card'
 import { useCheckIfThisColumnIsTheFirst } from '@/modules/columnList/hooks/useCheckIfThisColumnIsTheFirst'
@@ -6,6 +6,7 @@ import { useCheckIfThisColumnIsTheLast } from '@/modules/columnList/hooks/useChe
 
 import { useTheme } from '@/sharedByModules/Theme/ThemeContext'
 import { ColumnsContext } from '../ColumnsContext'
+import { taskModel } from '@/modules/taskList/models/task'
 
 const ColumnContext = createContext(columnNull)
 
@@ -16,17 +17,39 @@ interface ColumnProps {
 
 export function Column({ data, children }: ColumnProps) {
 	const colorTheme = useTheme()
-	const columnClassName = `h-auto min-w-48 flex-1 flex flex-col justify-between rounded-lg ${colorTheme.column}`
-	return (
-		<ColumnContext.Provider value={data}>
-			<Card className={columnClassName}>
-				<CardHeader>
-					<CardTitle>{data.name}</CardTitle>
-				</CardHeader>
+	const [ dragOver, setDragOver] = useState(false)
+	const columnClassName = `h-auto min-w-48 flex-1 flex flex-col justify-between rounded-lg ${colorTheme.column} ${dragOver && colorTheme.task}`
+	
+	const handleDrop = (e: DragEvent) => {
+		setDragOver(false)
+		const dropData = e.dataTransfer.getData('task')
+		if(dropData != null) {
+			const taskDragged: taskModel = JSON.parse(dropData)
+			console.log(taskDragged)
+		}
+	}
 
-				{children}
-			</Card>
-		</ColumnContext.Provider>
+	const handleDragOver = (e: DragEvent) => {
+		e.preventDefault()
+		setDragOver(true)
+	}
+
+	return (
+		<div 
+			className="p-0 m-0" 
+			onDragOver={handleDragOver}
+			onDragLeave={() => { setDragOver(false) }}
+			onDrop={handleDrop}
+		>
+			<ColumnContext.Provider value={data}>
+				<Card className={columnClassName}>
+					<CardHeader>
+						<CardTitle>{data.name}</CardTitle>
+					</CardHeader>
+					{children}
+				</Card>
+			</ColumnContext.Provider>
+		</div>
 	)
 }
 
