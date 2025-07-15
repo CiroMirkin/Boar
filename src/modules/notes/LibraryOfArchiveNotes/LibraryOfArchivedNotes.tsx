9f0860d48e3be6710dbaa640c208bb8bcf9a6115
CiroMirkin <ciromirkin@gmail.com>
2025-07-14 21:32:07 -0300

    agregado: componente ListOfArchivedNotes que quita responsabilidad al componente LibraryOfArchivedNotes

--- src/modules/notes/LibraryOfArchiveNotes/LibraryOfArchivedNotes.tsx
+++ src/modules/notes/LibraryOfArchiveNotes/LibraryOfArchivedNotes.tsx
@@ -1,35 +1,12 @@
-import { ReactNode, useEffect } from "react"
-import { LibraryOfArchivedNotes as LibraryOfArchiveNotesModel } from "./model/libraryOfArchivedNotes"
-import ArchivedNote from "./components/ArchivedNote"
 import { useTheme } from "@/App"
-import { useSession } from "@/SessionProvider"
-import { useSaveLibraryOfArchivedNotes } from "./repository/useSaveLibraryOfArchivedNotes"
+import { ListOfArchivedNotes } from "./components/ListOfArchivedNotes"
 
-export default function LibraryOfArchiveNotes({ libraryOfArchiveNotes }: { libraryOfArchiveNotes : LibraryOfArchiveNotesModel}) {
-
-    const { session } = useSession()
-    useEffect(() => {
-        useSaveLibraryOfArchivedNotes({
-            notes: libraryOfArchiveNotes,
-            session
-        })
-    }, [libraryOfArchiveNotes])
-
-    const archivedNotesList: ReactNode[] = libraryOfArchiveNotes.archive.reverse().map(note => (
-        <div className="p-0 m-0" key={note.id}>
-            <ArchivedNote note={note} />
-        </div>
-    ))
-
+export default function LibraryOfArchiveNotes() {
     const { bg } = useTheme()
     const className = `w-full h-full p-4 flex flex-col gap-4 items-center ${bg}`
     return (
         <main className={className}>
-            { 
-                libraryOfArchiveNotes.archive.length == 0
-                && <p className="opacity-50 text-lg">Aún no hay notar archivadas.</p>
-            }
-            { archivedNotesList }
+            <ListOfArchivedNotes />
         </main>
     )
 }
---
+++ src/modules/notes/LibraryOfArchiveNotes/components/ListOfArchivedNotes.tsx
@@ -0,0 +1,23 @@
+import { ReactNode } from "react"
+import ArchivedNote from "./ArchivedNote"
+import { useLibraryOfArchivedNotes } from "../state/useLibraryOfArchivedNotes"
+import { ArchivedNote as ArchivedNoteModel} from "../model/archivedNote"
+
+export function ListOfArchivedNotes() {
+    const archivedNotes = useLibraryOfArchivedNotes().archive
+    const archivedNotesList: ReactNode[] = archivedNotes.map((note: ArchivedNoteModel) => (
+        <div className="p-0 m-0" key={note.id}>
+            <ArchivedNote note={note} />
+        </div>
+    ))
+
+    return (
+        <>
+            { 
+                archivedNotes.length == 0
+                && <p className="opacity-50 text-lg">Aún no hay notar archivadas.</p>
+            }
+            { archivedNotesList }
+        </>
+    )
+}
