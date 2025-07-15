4fc30865109bde359aed6fe278353a7fd2a4ba10
CiroMirkin <ciromirkin@gmail.com>
2025-07-14 21:25:06 -0300

    cambio: se dividieron las responsabilidades entre los componentes Archive y ArchivePage

--- src/components/ArchivePage.tsx
+++ src/components/ArchivePage.tsx
@@ -1,9 +1,16 @@
+import { useTheme } from "@/App";
 import { Archive } from "@/modules/taskList/archive/Archive";
+import { Header } from "@/sharedByModules/Header/Header";
+import { USER_IS_IN } from "@/sharedByModules/Header/userIsIn";
+import { useTranslation } from "react-i18next";
 
 export function ArchivePage() {
+    const { bg } = useTheme()
+    const { t } = useTranslation()
     return (
-        <>
+        <div className={bg}>
+			<Header title={t('menu.archive')} whereUserIs={USER_IS_IN.ARCHIVE} />
             <Archive />
-        </>
+        </div>
     )
 }
