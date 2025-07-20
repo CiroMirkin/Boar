106c74d62bd46c683cfe5f88d6bf197bbd9a0292
CiroMirkin <ciromirkin@gmail.com>
2025-07-20 15:28:10 -0300

    cambio: estilos del componente LibraryOfArchivedNotes

--- src/modules/notes/LibraryOfArchiveNotes/LibraryOfArchivedNotes.tsx
+++ src/modules/notes/LibraryOfArchiveNotes/LibraryOfArchivedNotes.tsx
@@ -3,7 +3,7 @@ import { ListOfArchivedNotes } from "./components/ListOfArchivedNotes"
 
 export default function LibraryOfArchiveNotes() {
     const { bg } = useTheme()
-    const className = `w-full h-full p-4 flex flex-col gap-4 items-center ${bg}`
+    const className = `w-full min-h-[calc(100vh-7.8rem)] p-4 flex flex-col gap-4 items-center ${bg}`
     return (
         <main className={className}>
             <ListOfArchivedNotes />
