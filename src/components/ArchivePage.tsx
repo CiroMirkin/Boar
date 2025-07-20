681cbb0b00145178da893096f6a97eb0daa587cd
CiroMirkin <ciromirkin@gmail.com>
2025-07-20 15:29:20 -0300

    cambio: estilos del componente ArchivePage e implementacion del componente LibraryOfArchiveNotes

--- src/components/ArchivePage.tsx
+++ src/components/ArchivePage.tsx
@@ -1,16 +1,29 @@
 import { useTheme } from "@/App";
+import LibraryOfArchiveNotes from "@/modules/notes/LibraryOfArchiveNotes/LibraryOfArchivedNotes";
 import { Archive } from "@/modules/taskList/archive/Archive";
 import { Header } from "@/sharedByModules/Header/Header";
 import { USER_IS_IN } from "@/sharedByModules/Header/userIsIn";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/ui/tabs";
 import { useTranslation } from "react-i18next";
 
 export function ArchivePage() {
     const { bg } = useTheme()
     const { t } = useTranslation()
     return (
-        <div className={bg}>
+        <div className={bg + " flex flex-col items-center"}>
 			<Header title={t('menu.archive')} whereUserIs={USER_IS_IN.ARCHIVE} />
-            <Archive />
+            <Tabs defaultValue="tareas-archivadas">
+                <TabsList className="self-center justify-self-stretch">
+                    <TabsTrigger value="tareas-archivadas" >Tareas</TabsTrigger>
+                    <TabsTrigger value="notas-archivadas" >Notas</TabsTrigger>
+                </TabsList>
+                <TabsContent value="tareas-archivadas">
+                    <Archive />
+                </TabsContent>
+                <TabsContent value="notas-archivadas">
+                    <LibraryOfArchiveNotes />
+                </TabsContent>
+            </Tabs>
         </div>
     )
 }
