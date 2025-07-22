aab19aac02d6c9f63c70fca31ecc44eece1648fd
CiroMirkin <ciromirkin@gmail.com>
2025-07-21 21:58:20 -0300

    cambio: min-h en el componente Auth

--- src/components/Auth.tsx
+++ src/components/Auth.tsx
@@ -95,7 +95,7 @@ export default function Auth() {
   return (
     <div className={`${bg} ${text}`}>
       <Header title='Boar' whereUserIs={USER_IS_IN.AUTH} />
-      <main className='w-full min-h-[calc(100vh-6rem)] grid place-items-center'>
+      <main className='w-full min-h-[calc(100vh-5rem)] grid place-items-center'>
         <Card className='rounded-lg px-6 py-2'>
           <CardHeader>
             <CardTitle>{isRegister ? t('log_in_form_title') : t('sing_in')}</CardTitle>
