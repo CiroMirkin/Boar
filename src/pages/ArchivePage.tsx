8f62be1c925094e413c7e552065d73877b0377af
CiroMirkin <ciromirkin@gmail.com>
2025-07-27 16:31:20 -0300

    cambio: implementacion del componente PageContainer en el componente ArchivePage

--- src/pages/ArchivePage.tsx
+++ src/pages/ArchivePage.tsx
@@ -1,17 +1,18 @@
-import { useTheme } from "@/App";
 import LibraryOfArchiveNotes from "@/modules/notes/LibraryOfArchiveNotes/LibraryOfArchivedNotes";
 import { Archive } from "@/modules/taskList/archive/Archive";
-import { Header } from "@/sharedByModules/Header/Header";
 import { USER_IS_IN } from "@/sharedByModules/Header/userIsIn";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/ui/tabs";
 import { useTranslation } from "react-i18next";
+import PageContainer from "./PageContainer";
 
 export function ArchivePage() {
-    const { bg } = useTheme()
     const { t } = useTranslation()
     return (
-        <div className={bg + " flex flex-col items-center"}>
-			<Header title={t('menu.archive')} whereUserIs={USER_IS_IN.ARCHIVE} />
+        <PageContainer 
+            title={t('menu.archive')} 
+            whereUserIs={USER_IS_IN.ARCHIVE} 
+            className="flex flex-col items-center"
+        >
             <Tabs defaultValue="tareas-archivadas">
                 <TabsList className="self-center justify-self-stretch">
                     <TabsTrigger value="tareas-archivadas" >{ t('archive_page.tasks') }</TabsTrigger>
@@ -24,6 +25,6 @@ export function ArchivePage() {
                     <LibraryOfArchiveNotes />
                 </TabsContent>
             </Tabs>
-        </div>
+        </PageContainer>
     )
 }
