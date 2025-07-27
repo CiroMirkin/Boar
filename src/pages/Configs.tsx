42b236292d9dd906d6d2eccf7996f88efb993fde
CiroMirkin <ciromirkin@gmail.com>
2025-07-27 16:14:07 -0300

    cambio: implementacion del componente PageContainer en el componente COnfigs

--- src/pages/Configs.tsx
+++ src/pages/Configs.tsx
@@ -1,51 +1,41 @@
 import { useTranslation } from 'react-i18next'
-import { Header } from '../sharedByModules/Header/Header'
 import { USER_IS_IN } from '../sharedByModules/Header/userIsIn'
 import { useTheme } from "@/App"
-
 import { ChangeBoardName } from '../modules/board/components/ChangeBoardName'
 import { ConfigColumns } from '../modules/columnList/components/ConfigColumns'
 import CreateReminder from '../modules/taskList/Reminder/CreateReminder'
 import { ThemeSelection } from '../modules/Theme/ThemeSelection'
 import { useColumnList } from '@/modules/columnList/hooks/useColumnList'
 import { ReminderList } from '@/modules/taskList/Reminder/ReminderList'
+import PageContainer from './PageContainer'
 
 export function Configs() {
 	const { t } = useTranslation()
-	const { bg, text } = useTheme()
-
-	return (
-		<>
-			<div className={`${bg} ${text}`}>
-				<Header title={t('menu.configs')} whereUserIs={USER_IS_IN.CONFIG} />
-				<div className="px-3 pb-6 grid justify-items-center">
-					<div className="grid gap-4 justify-items-stretch ">
-						<ConfigPageContent />
-					</div>
-				</div>
-			</div>
-		</>
-	)
-}
-
-const ConfigPageContent = () => {
 	const { column } = useTheme()
 	const sectionClassName = `max-w-2xl rounded-lg py-4 md:px-11 px-6 ${column}`
+
 	return (
-		<>
-			<div className={sectionClassName}>
-				<ChangeBoardName />
+		<PageContainer 
+			title={t('menu.configs')} 
+			whereUserIs={USER_IS_IN.CONFIG} 
+			className='px-3 pb-6'
+		>
+			<div className="grid gap-4 justify-items-stretch ">
+				<div className={sectionClassName}>
+					<ChangeBoardName />
+				</div>
+				<div className={sectionClassName}>
+					<ConfigColumns />
+				</div>
+				<div className={sectionClassName}>
+					<CreateReminder columnList={ useColumnList() } />
+					<ReminderList />
+				</div>
+				<div className={sectionClassName}>
+					<ThemeSelection />
+				</div>
 			</div>
-			<div className={sectionClassName}>
-				<ConfigColumns />
-			</div>
-			<div className={sectionClassName}>
-				<CreateReminder columnList={ useColumnList() } />
-				<ReminderList />
-			</div>
-			<div className={sectionClassName}>
-				<ThemeSelection />
-			</div>
-		</>
+		</PageContainer> 
 	)
 }
+
