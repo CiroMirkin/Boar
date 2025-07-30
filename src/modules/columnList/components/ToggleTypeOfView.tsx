729c2ff7ad78e73c67547453e93e1956e6a47647
CiroMirkin <ciromirkin@gmail.com>
2025-07-30 18:07:42 -0300

    agregado: internacionalizacion para el componente ToggleTypeOfView

--- src/modules/columnList/components/ToggleTypeOfView.tsx
+++ src/modules/columnList/components/ToggleTypeOfView.tsx
@@ -4,21 +4,23 @@ import { toggleView } from "../state/columnListReducer";
 import { useTypeOfView } from "../hooks/useTypeOfView";
 import { toast } from "sonner";
 import { ColumnsIcon } from "@/ui/icons";
+import { useTranslation } from "react-i18next";
 
 export function ToggleTypeOfView() {
     const actualTypeOfView = useTypeOfView()
+    const { t } = useTranslation()
 
     const dispatch = useDispatch()
     const handleValueChange = () => {
         dispatch(toggleView())
-        toast.success('¡El tipo de vista se cambió con éxito!')
+        toast.success(t('settings.type_of_view.successful_toast'))
     }
 
     return (
         <div className="w-full flex flex-col gap-y-3">
 			<header>
-                <h2 className='text-2xl'>Tipo de vista</h2>
-                <p>Esta será la forma en la que se verá su tablero.</p>
+                <h2 className='text-2xl'>{ t('settings.type_of_view.section_title') }</h2>
+                <p className="opacity-75">{ t('settings.type_of_view.section_description') }</p>
             </header>
             <main className="flex gap-2 items-center">
                 <div>
@@ -29,11 +31,11 @@ export function ToggleTypeOfView() {
                 </div>
                 <Select onValueChange={handleValueChange} defaultValue={actualTypeOfView} >
                     <SelectTrigger className="w-[180px]" >
-                        <SelectValue placeholder="Tipo de vista" />
+                        <SelectValue placeholder={ t('settings.type_of_view.section_title') } />
                     </SelectTrigger>
                     <SelectContent>
-                        <SelectItem value="BOARD">Tablero</SelectItem>
-                        <SelectItem value="LIST">Lista</SelectItem>
+                        <SelectItem value="BOARD">{ t('settings.type_of_view.board_option_view') }</SelectItem>
+                        <SelectItem value="LIST">{ t('settings.type_of_view.list_option_view') }</SelectItem>
                     </SelectContent>
                 </Select>
             </main>
