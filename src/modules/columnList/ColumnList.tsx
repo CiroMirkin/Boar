9f30a0cece49c06f49060f6e4c1494accee41390
Ciro Mirkin <107360995+CiroMirkin@users.noreply.github.com>
2025-07-30 18:23:34 +0000

    agregado: implementacion del componente ListView

--- src/modules/columnList/ColumnList.tsx
+++ src/modules/columnList/ColumnList.tsx
@@ -1,13 +1,27 @@
 import { ColumnsContent } from './components/ListOfColumns'
 import { ColumnsContextContent, ColumnsProvider } from './ColumnsContext'
 import { TableView } from './components/TableView'
+import { ListView } from './components/ListView'
+
+export type TypeOfView = 'BOARD' | 'LIST'
 
 interface ColumnListProps {
 	children: () => ColumnsContent, 
 	columnsData: ColumnsContextContent,
+	typeOfView?: TypeOfView,
 }
 
-function ColumnList({ children, columnsData }: ColumnListProps) {
+function ColumnList({ children, columnsData, typeOfView = 'BOARD' }: ColumnListProps) {
+	if(typeOfView == 'LIST') {
+		return (
+			<ColumnsProvider value={columnsData}>
+				<ListView>
+					{ children }
+				</ListView>
+			</ColumnsProvider>
+		)
+	}
+
 	return (
 		<ColumnsProvider value={columnsData}>
 			<TableView>
