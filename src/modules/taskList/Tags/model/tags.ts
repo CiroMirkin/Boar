9040cf00b15f83a8e97f546c1345b3bf37d4e3fe
CiroMirkin <ciromirkin@gmail.com>
2025-08-18 19:14:24 -0300

    agregado: modelo del modulo Tags

---
+++ src/modules/taskList/Tags/model/tags.ts
@@ -0,0 +1,37 @@
+
+export interface Tag {
+    id: string
+    name: string
+}
+
+const defaultTagList: Tag[] = [
+    {
+        id: '1',
+        name: 'Importante',
+    }, 
+    {
+        id: '2',
+        name: 'Necesario',
+    },
+]
+
+export interface TagGroup {
+    id: string
+    tags: Tag[]
+}
+
+export type AvailableTags = TagGroup[]
+
+export const defaultAvialableTags: AvailableTags = [
+    {
+        id: 'default',
+        tags: [...defaultTagList]
+    }
+]
+
+export const getTagGroup = ({ tags }: { tags: Tag[] }): TagGroup => {
+    return {
+        id: crypto.randomUUID(),
+        tags: [...tags]
+    }
+} 
