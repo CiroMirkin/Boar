6a7d8da50c57483064b75650a2ce41e560606577
CiroMirkin <ciromirkin@gmail.com>
2025-08-19 23:32:05 -0300

    agregado: componente TagGroupSelect

---
+++ src/modules/taskList/Tags/components/TagGroupSelect.tsx
@@ -0,0 +1,55 @@
+import { CheckboxBadge } from "@/ui/molecules/CheckboxBadge"
+import { useEffect, useState } from "react"
+import { useActualTagGroup } from "../hooks/useActualTagGroup"
+import { useDispatch } from "react-redux"
+import { Tag } from "../model/tags"
+import { setUserSelectedTags } from "../state/tagsReducer"
+import { useUserSelectedTags } from "../hooks/useUserSelectedTags"
+
+export default function TagGroupSelect() {
+    const [selectedTags, setSelectedTags] = useState<string[]>([])
+    const actualTagGroup = useActualTagGroup()
+
+    const getFullSelectedTags = (listOfTagIds: string[]): Tag[] => {
+        return actualTagGroup.tags.filter(tag => listOfTagIds.includes(tag.id))
+    }
+
+    const dispatch = useDispatch()
+    const handleCheckboxChange = (checked: boolean, id: string) => {
+        setSelectedTags(prev => {
+            if (checked) {
+                const newSelectedTags = [...prev, id]
+                const tags = getFullSelectedTags(newSelectedTags)
+                dispatch(setUserSelectedTags(tags))
+                return newSelectedTags
+            } 
+            const newSelectedTags = prev.filter(item => item !== id)
+            const tags = getFullSelectedTags(newSelectedTags)
+            dispatch(setUserSelectedTags(tags))
+            return newSelectedTags
+        })
+    }
+
+    const userSelectedTags = useUserSelectedTags()
+    useEffect(() => {
+        if(userSelectedTags.length == 0) {
+            setSelectedTags([])
+        }
+    }, [userSelectedTags])
+
+    return (
+        <div className="flex flex-wrap gap-3">
+            { actualTagGroup.tags.map(tag => (
+                <CheckboxBadge
+                    id={tag.id}
+                    key={tag.id}
+                    variant="inverted"
+                    checked={selectedTags.filter(selectedTag => selectedTag == tag.id)[0] ? true : false}
+                    onChange={handleCheckboxChange}
+                >
+                    {tag.name}
+                </CheckboxBadge>
+            ))}
+        </div>
+    )
+}
