3085a96fda1e84bf5863c4228ae9a20895b084ca
Ciro Mirkin <107360995+CiroMirkin@users.noreply.github.com>
2025-09-01 23:33:52 +0000

    refactoriza: useSaveArchive y su implementacion

--- src/modules/taskList/ArchivedTasks/state/useSaveArchive.tsx
+++ src/modules/taskList/ArchivedTasks/state/useSaveArchive.tsx
@@ -9,20 +9,22 @@ interface useSaveArchiveParams {
 	emptyArchive?: boolean
 }
 
-export const useSaveArchive = ({
-	archive,
-	session,
-	emptyArchive = false,
-}: useSaveArchiveParams) => {
+export const useSaveArchive = () => {
 	const localStorage = new LocalStorageArchiveRepository()
 	const localArchive = localStorage.getAll()
 
-	if (JSON.stringify(archive) !== JSON.stringify([]) || emptyArchive) {
-		const isNotTheLocalArchive = JSON.stringify(archive) !== JSON.stringify(localArchive)
-		if (!!session && isNotTheLocalArchive) {
-			sendForSaveArchive(archive)
-		} else {
-			localStorage.save(archive)
+	return ({
+		archive,
+		session,
+		emptyArchive = false,
+	}: useSaveArchiveParams) => {
+		if (JSON.stringify(archive) !== JSON.stringify([]) || emptyArchive) {
+			const isNotTheLocalArchive = JSON.stringify(archive) !== JSON.stringify(localArchive)
+			if (!!session && isNotTheLocalArchive) {
+				sendForSaveArchive(archive)
+			} else {
+				localStorage.save(archive)
+			}
 		}
 	}
 }
