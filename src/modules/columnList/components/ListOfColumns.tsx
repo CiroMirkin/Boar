9ca9e39d84aee5c53d9f7718b8506870bcb708a7
Ciro Mirkin <ciromirkin@gmail.com>
2025-09-02 15:39:38 -0300

    refactoriza: useSaveColumnList

--- src/modules/columnList/components/ListOfColumns.tsx
+++ src/modules/columnList/components/ListOfColumns.tsx
@@ -1,7 +1,5 @@
-import { useSession } from '@/SessionProvider'
 import { Column } from './Column'
 import { useColumnList } from '../hooks/useColumnList'
-import { useEffect } from 'react'
 import { useSaveColumnList } from '../state/useSaveColumnList'
 
 export type ColumnsContent = React.ReactNode[]
@@ -12,11 +10,7 @@ interface ListOfColumnProps {
 
 export function ListOfColumn({ children: getColumnsContent }: ListOfColumnProps) {
 	const columns = useColumnList()
-
-	const { session } = useSession()
-	useEffect(() => {
-		useSaveColumnList({ session, columnList: columns })
-	}, [columns])
+	useSaveColumnList({ columnList: columns })
 
 	const columnsContent = getColumnsContent()
 	const columnList = columns.map((column, columnIndex) => {
