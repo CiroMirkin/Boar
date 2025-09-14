44c997f2cb977150e98f0e5bdb1d48ea1b34afb8
Ciro Mirkin <ciromirkin@gmail.com>
2025-09-14 16:55:11 -0300

    cambio: formato del codigo

--- src/sharedByModules/hooks/useDeleteColumn.tsx
+++ src/sharedByModules/hooks/useDeleteColumn.tsx
@@ -5,31 +5,31 @@ import { deleteColumn } from '@/modules/columnList/state/columnListReducer'
 import { RootState } from '@/store'
 
 export const useDeleteColumn = () => {
-    const updateBoardData = useDispatch()
-    const { list: taskListInEachColumn } = useSelector(
-        (state: RootState) => state.taskListInEachColumn
-    )
+	const updateBoardData = useDispatch()
+	const { list: taskListInEachColumn } = useSelector(
+		(state: RootState) => state.taskListInEachColumn
+	)
 
-    return ({ column }: { column: Column }) => {
-        const deletedColumnIndex = getIndexOfColumnInColumnList(column.position)
+	return ({ column }: { column: Column }) => {
+		const deletedColumnIndex = getIndexOfColumnInColumnList(column.position)
 
-        // Actualizacion de las posiciones de las tareas en las columnas restantes para mantener la consistencia de los datos.
-        const newTaskListInEachColumn = taskListInEachColumn
-            .filter((_, index) => index !== deletedColumnIndex)
-            .map((taskList) => {
-                return taskList.map((task) => {
-                    const taskColumnPosition = Number(task.columnPosition)
-                    if (taskColumnPosition > deletedColumnIndex) {
-                        return {
-                            ...task,
-                            columnPosition: String(taskColumnPosition - 1),
-                        }
-                    }
-                    return task
-                })
-            })
+		// Actualizacion de las posiciones de las tareas en las columnas restantes para mantener la consistencia de los datos.
+		const newTaskListInEachColumn = taskListInEachColumn
+			.filter((_, index) => index !== deletedColumnIndex)
+			.map((taskList) => {
+				return taskList.map((task) => {
+					const taskColumnPosition = Number(task.columnPosition)
+					if (taskColumnPosition > deletedColumnIndex) {
+						return {
+							...task,
+							columnPosition: String(taskColumnPosition - 1),
+						}
+					}
+					return task
+				})
+			})
 
-        updateBoardData(deleteColumn(column))
-        updateBoardData(setTaskListInEachColumn(newTaskListInEachColumn))
-    }
+		updateBoardData(deleteColumn(column))
+		updateBoardData(setTaskListInEachColumn(newTaskListInEachColumn))
+	}
 }
