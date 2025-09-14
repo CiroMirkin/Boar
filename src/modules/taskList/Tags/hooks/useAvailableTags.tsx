0028452bf48f9c8cf3bc898c17bd99f12da37a8b
Ciro Mirkin <ciromirkin@gmail.com>
2025-09-14 17:13:20 -0300

    cambio: mejora en la traduccion de las etiquetas

--- src/modules/taskList/Tags/hooks/useAvailableTags.tsx
+++ src/modules/taskList/Tags/hooks/useAvailableTags.tsx
@@ -1,34 +1,36 @@
 import { useSelector } from 'react-redux'
 import { RootState } from '@/store'
-import { AvailableTags } from '../model/tags'
+import { AvailableTags, TagGroup } from '../model/tags'
 import { eisenhowerTagGroup } from '../model/defaultTags'
 import { useTranslation } from 'react-i18next'
+import { TFunction } from 'i18next'
+
+export const translateTagGroup = (tagGroup: TagGroup, t: TFunction) => {
+	if (tagGroup.id == eisenhowerTagGroup.id) {
+		const [importantTag, necessaryTag, urgentTeg] = tagGroup.tags
+		return {
+			...tagGroup,
+			tags: [
+				{
+					...importantTag,
+					name: t('tags.important_tag'),
+				},
+				{
+					...necessaryTag,
+					name: t('tags.necessary_tag'),
+				},
+				{
+					...urgentTeg,
+					name: t('tags.urgent_tag'),
+				},
+			],
+		}
+	}
+	return tagGroup
+}
 
 export const useAvailableTags = (): AvailableTags => {
 	const availableTags = useSelector((state: RootState) => state.tags.list)
 	const { t } = useTranslation()
-	return availableTags.map((tagGroup) => {
-		// Se traducen las Tags por defecto
-		if (tagGroup.id == eisenhowerTagGroup.id) {
-			const [importantTag, necessaryTag, urgentTeg] = tagGroup.tags
-			return {
-				...tagGroup,
-				tags: [
-					{
-						...importantTag,
-						name: t('tags.important_tag'),
-					},
-					{
-						...necessaryTag,
-						name: t('tags.necessary_tag'),
-					},
-					{
-						...urgentTeg,
-						name: t('tags.urgent_tag'),
-					},
-				],
-			}
-		}
-		return tagGroup
-	})
+	return availableTags.map((tagGroup) => translateTagGroup(tagGroup, t))
 }
