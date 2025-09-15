e5850d19e1734e4bd80b06e6d4d507227b6567a2
Ciro Mirkin <ciromirkin@gmail.com>
2025-09-15 14:12:33 -0300

    agrega: mejora en la traduccion del nombre de las columnas por defecto

--- src/modules/columnList/hooks/useColumnList.tsx
+++ src/modules/columnList/hooks/useColumnList.tsx
@@ -3,28 +3,31 @@ import { useSelector } from 'react-redux'
 import { Column } from '../models/column'
 import { defaultColumnList } from '../models/columnList'
 import { useTranslation } from 'react-i18next'
+import { useMemo } from 'react'
 
 export const useColumnList = (): Column[] => {
 	const { t } = useTranslation()
 	const columnList = useSelector((state: RootState) => state.columnList.list)
 
-	const isTheColumnListByDefault = columnList.every(
-		(id, index) => id === defaultColumnList[index]
-	)
+	return useMemo(() => {
+		const isTheColumnListByDefault =
+			columnList.length === defaultColumnList.length &&
+			columnList.every((column, index) => column.id === defaultColumnList[index].id)
 
-	if (!isTheColumnListByDefault) {
-		return columnList
-	}
-
-	const translateColumnName = columnList.map((column) => {
-		const key = `default_columns.${column.id}`
-		const translatedName = t(key)
-
-		return {
-			...column,
-			name: translatedName,
+		if (!isTheColumnListByDefault) {
+			return columnList
 		}
-	})
 
-	return translateColumnName
+		const translateColumnName = columnList.map((column) => {
+			const key = `default_columns.${column.id}`
+			const translatedName = t(key)
+
+			return {
+				...column,
+				name: translatedName,
+			}
+		})
+
+		return translateColumnName
+	}, [columnList, t])
 }
