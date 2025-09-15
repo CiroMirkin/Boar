a88d7a07177df1f1fa2238f22640037048fa5fd3
Ciro Mirkin <ciromirkin@gmail.com>
2025-09-15 13:59:01 -0300

    agrega: funcion isDefaultBoardName

--- src/modules/board/models/board.ts
+++ src/modules/board/models/board.ts
@@ -1,4 +1,5 @@
 import BusinessError from '@/sharedByModules/errors/businessError'
+import i18next from '@/i18next'
 
 export interface boardModel {
 	id: string
@@ -19,4 +20,12 @@ export const isThisBoardNameValid = (boardName: string): boolean | BusinessError
 	else if (!isThisBoardNameWithinTheLimitOfLetters(boardName))
 		throw new BusinessError('El nombre es demasiado largo.')
 	return true
+}
+
+export const isDefaultBoardName = (boardName: string): boolean => {
+    const translations = i18next.services.resourceStore.data
+    const boardNameTranslations = Object.values(translations).map(
+        (lang) => (lang.translation as { board_name: string }).board_name
+    )
+    return boardNameTranslations.includes(boardName)
 }
