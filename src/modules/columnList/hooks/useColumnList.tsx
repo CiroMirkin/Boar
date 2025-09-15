e15703dad44ee777abc45f920f4735f00ab57e55
Ciro Mirkin <ciromirkin@gmail.com>
2025-09-14 22:04:08 -0300

    agrega: traduccion de los nombre de las columnas por defecto en useColumnList

--- src/modules/columnList/hooks/useColumnList.tsx
+++ src/modules/columnList/hooks/useColumnList.tsx
@@ -1,7 +1,30 @@
-import { ColumnList } from '@/modules/columnList/models/columnList'
 import { RootState } from '@/store'
 import { useSelector } from 'react-redux'
+import { Column } from '../models/column'
+import { defaultColumnList } from '../models/columnList'
+import { useTranslation } from 'react-i18next'
 
-export const useColumnList = (): ColumnList => {
-	return useSelector((state: RootState) => state.columnList.list)
+export const useColumnList = (): Column[] => {
+	const { t } = useTranslation()
+	const columnList = useSelector((state: RootState) => state.columnList.list)
+
+	const isTheColumnListByDefault = columnList.every(
+		(id, index) => id === defaultColumnList[index]
+	)
+
+	if (!isTheColumnListByDefault) {
+		return columnList
+	}
+
+	const translateColumnName = columnList.map((column) => {
+		const key = `default_columns.${column.id}`
+		const translatedName = t(key)
+
+		return {
+			...column,
+			name: translatedName,
+		}
+	})
+
+	return translateColumnName
 }
