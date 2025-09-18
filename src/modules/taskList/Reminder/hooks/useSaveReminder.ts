90e78f5eb34779bf09e2cf8233677ba4ffb41c84
Ciro Mirkin <ciromirkin@gmail.com>
2025-09-17 23:22:30 -0300

    agrega: implementacion de saveReminderInSupabase en useSaveReminder

--- src/modules/taskList/Reminder/hooks/useSaveReminder.ts
+++ src/modules/taskList/Reminder/hooks/useSaveReminder.ts
@@ -2,13 +2,27 @@ import LocalStorageReminderRepository from '../state/LocalStorageReminder'
 import { Reminder } from '../reminder'
 import { useDispatch } from 'react-redux'
 import { setReminder } from '../state/reminderReducer'
+import { useSession } from '@/SessionProvider'
+import { saveReminderInSupabase } from '../state/saveReminderInSupabase'
 
 type UseSaveReminderReturn = (reminder: Reminder) => void
 
 export const useSaveReminder = (): UseSaveReminderReturn => {
+	const { session } = useSession()
+	const saveInRepository = (reminder: Reminder) => {
+		if (session) {
+			const localReminder = new LocalStorageReminderRepository().getAll()
+			if (JSON.stringify(localReminder) !== JSON.stringify(reminder)) {
+				saveReminderInSupabase(reminder)
+			}
+		} else {
+			new LocalStorageReminderRepository().save(reminder)
+		}
+	}
+
 	const dispatch = useDispatch()
 	return (reminder: Reminder) => {
 		dispatch(setReminder(reminder))
-		new LocalStorageReminderRepository().save(reminder)
+		saveInRepository(reminder)
 	}
 }
