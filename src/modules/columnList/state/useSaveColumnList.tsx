0fdc8c6a12dd6781fd75179772ccddc627fc299f
Ciro Mirkin <ciromirkin@gmail.com>
2025-09-20 16:43:04 -0300

    fix: error al crear y modificar columnas

--- src/modules/columnList/state/useSaveColumnList.tsx
+++ src/modules/columnList/state/useSaveColumnList.tsx
@@ -2,7 +2,7 @@ import { useSession } from '@/SessionProvider'
 import { ColumnList, defaultColumnList } from '../models/columnList'
 import LocalStorageColumnListRepository from './localStorageColumnList'
 import { sendForSaveColumnList } from './sendForSaveColumnList'
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 
 interface useSaveColumnListParams {
 	columnList: ColumnList
@@ -10,14 +10,21 @@ interface useSaveColumnListParams {
 
 export const useSaveColumnList = ({ columnList }: useSaveColumnListParams) => {
 	const { session } = useSession()
+	const columnListRef = useRef(columnList)
 	useEffect(() => {
 		const localStorage = new LocalStorageColumnListRepository()
 		const localColumnList = localStorage.getAll()
 
-		if (JSON.stringify(columnList) !== JSON.stringify(defaultColumnList)) {
+		const isNotColumnListByDefault =
+			JSON.stringify(columnList) !== JSON.stringify(defaultColumnList)
+		const beforeWasNotColumnListByDefault =
+			JSON.stringify(columnListRef.current) !== JSON.stringify(defaultColumnList)
+
+		if (isNotColumnListByDefault || beforeWasNotColumnListByDefault) {
 			const isNotTheLocalColumnList =
 				JSON.stringify(columnList) !== JSON.stringify(localColumnList)
-			if (!!session && isNotTheLocalColumnList) {
+			columnListRef.current = columnList
+			if (session && isNotTheLocalColumnList) {
 				sendForSaveColumnList(columnList)
 			} else {
 				localStorage.save(columnList)
