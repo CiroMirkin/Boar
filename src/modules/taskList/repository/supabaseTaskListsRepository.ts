bb869925c272baa76319aaeaa93ab3225bb94089
Ciro Mirkin <ciromirkin@gmail.com>
2025-09-19 21:22:02 -0300

    cambia: formato del codigo

--- src/modules/taskList/repository/supabaseTaskListsRepository.ts
+++ src/modules/taskList/repository/supabaseTaskListsRepository.ts
@@ -4,30 +4,30 @@ import { getUserId } from '@/sharedByModules/hooks/useSyncBoard'
 import { supabase } from '@/lib/supabase'
 
 export default class SupabaseTaskListInEachColumnRepository
-    implements TaskListInEachColumnRepository
+	implements TaskListInEachColumnRepository
 {
-    async save(taskListInEachColumn: TaskListInEachColumn) {
-        const user_id = await getUserId()
-        const { error } = await supabase
-            .from('boards')
-            .update({
-                task_list_in_each_column: taskListInEachColumn,
-            })
-            .eq('user_id', user_id)
-        
-        if (error) throw error
-    }
+	async save(taskListInEachColumn: TaskListInEachColumn) {
+		const user_id = await getUserId()
+		const { error } = await supabase
+			.from('boards')
+			.update({
+				task_list_in_each_column: taskListInEachColumn,
+			})
+			.eq('user_id', user_id)
 
-    async getAll() {
-        const user_id = await getUserId()
-        const { error, data } = await supabase
-            .from('boards')
-            .select('task_list_in_each_column')
-            .eq('user_id', user_id)
-        
-        if (error) throw error
+		if (error) throw error
+	}
 
-        const taskListInColumns = data[0].task_list_in_each_column
-        return taskListInColumns ? taskListInColumns : emptyTaskListInEachColumn
-    }
+	async getAll() {
+		const user_id = await getUserId()
+		const { error, data } = await supabase
+			.from('boards')
+			.select('task_list_in_each_column')
+			.eq('user_id', user_id)
+
+		if (error) throw error
+
+		const taskListInColumns = data[0].task_list_in_each_column
+		return taskListInColumns ? taskListInColumns : emptyTaskListInEachColumn
+	}
 }
