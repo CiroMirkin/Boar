64b1afe9d2a0a2f896cb63c3e62de61577427b9f
Ciro Mirkin <ciromirkin@gmail.com>
2025-09-20 17:24:59 -0300

    cambia: extrae la funcion saveUserBoardOnSupabase en un archivo propio

--- src/auth/hooks/useSyncBoard.tsx
+++ src/auth/hooks/useSyncBoard.tsx
@@ -3,17 +3,8 @@ import { setUserBoard } from '../utils/setUserBoard'
 import { Dispatch } from '@reduxjs/toolkit'
 import { Session } from '@supabase/supabase-js'
 import { Dispatch as ReactDispatch, SetStateAction } from 'react'
-import { UserBoardOnDB } from '../model/UserBoardOnDB'
 import { getActualUserBoard } from '../utils/getActualUserBoard'
-
-const saveUserBoardOnSupabase = async (userBoard: UserBoardOnDB) => {
-	try {
-		const { error } = await supabase.from('boards').insert(userBoard)
-		if (error) throw error
-	} catch (e) {
-		console.error(e)
-	}
-}
+import { saveUserBoardOnSupabase } from '../utils/saveUserBoardOnSupabase'
 
 /** Recupera el tablero del usuario de Supabase y si no existe ninguno guarda el tablero actual en Supabase. */
 export const syncBoard = async ({
