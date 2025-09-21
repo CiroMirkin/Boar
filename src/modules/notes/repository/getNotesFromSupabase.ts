303011683d337d3542fba49a2f81a3a355420cc9
Ciro Mirkin <ciromirkin@gmail.com>
2025-09-21 16:14:19 -0300

    agrega: implementacion de SupabaseNotesRepository

--- src/modules/notes/repository/getNotesFromSupabase.ts
+++ src/modules/notes/repository/getNotesFromSupabase.ts
@@ -1,15 +1,12 @@
-import { supabase } from '@/lib/supabase'
 import { Dispatch, SetStateAction } from 'react'
+import SupabaseNotesRepository from './SupabaseNotesRepository'
 
 interface getNotesFromSupabaseParams {
 	setNotes: Dispatch<SetStateAction<string>>
 }
 
 export const getNotesFromSupabase = async ({ setNotes }: getNotesFromSupabaseParams) => {
-	const { data } = await supabase.from('boards').select('notes')
-
-	if (data != null) {
-		const notes: string = data[0].notes
-		setNotes(notes)
-	}
+	const supabaseNotes = new SupabaseNotesRepository()
+	const notes = await supabaseNotes.getAll()
+	setNotes(notes)
 }
