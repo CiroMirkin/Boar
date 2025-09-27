902d55850a0841d0dd33f1b43fdf695493a6f26b
Ciro Mirkin <ciromirkin@gmail.com>
2025-09-26 23:49:16 -0300

    cambia: interfaz UserBoardOnSupabase y su implementacion

--- src/auth/utils/getActualUserBoard.ts
+++ src/auth/utils/getActualUserBoard.ts
@@ -1,17 +1,21 @@
 import LocalStorageNotesRepository from '@/modules/notes/repository/LocalStorageNotesRepository'
 import { store } from '@/store'
-import { UserBoardOnSupabase } from '../model/UserBoardOnSupabase'
 import { getUserId } from './getUserId'
+import { UserBoard } from '../model/UserBoard'
 
-export const getActualUserBoard = async (): Promise<UserBoardOnSupabase> => {
+export const getActualUserBoard = async (): Promise<UserBoard> => {
 	const notes = new LocalStorageNotesRepository().getAll()
 	return {
-		name: store.getState().board.board.name,
-		column_list: store.getState().columnList.list,
-		task_list_in_each_column: store.getState().taskListInEachColumn.list,
-		user_id: await getUserId(),
-		notes,
-		actual_tag_group: store.getState().tags.actualTagGroup,
-		reminders: store.getState().reminder.reminder,
+		board: {
+			name: store.getState().board.board.name,
+			column_list: store.getState().columnList.list,
+			task_list_in_each_column: store.getState().taskListInEachColumn.list,
+			user_id: await getUserId(),
+		},
+		accessories: {
+			notes,
+			actual_tag_group: store.getState().tags.actualTagGroup,
+			reminders: store.getState().reminder.reminder,
+		},
 	}
 }
