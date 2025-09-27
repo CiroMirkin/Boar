5c44f7629a47400ccc169005602ee42ace2c9362
Ciro Mirkin <ciromirkin@gmail.com>
2025-09-26 22:54:37 -0300

    agrega: interfaces UserBoard y UserBoardAccessotiesOnSupabase

---
+++ src/auth/model/UserBoardAccessoriesOnSupabase.ts
@@ -0,0 +1,9 @@
+import { Notes } from '@/modules/notes/model/notes'
+import { Reminder } from '@/modules/taskList/Reminder/reminder'
+import { TagGroup } from '@/modules/taskList/Tags/model/tags'
+
+export interface UserBoardAccessoriesOnSupabase {
+	notes: Notes
+	actual_tag_group: TagGroup
+	reminders: Reminder
+}
