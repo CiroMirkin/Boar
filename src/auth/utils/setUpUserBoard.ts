c0dd9f25f9608f447cdf5b6f0aabb3e1710ff273
Ciro Mirkin <ciromirkin@gmail.com>
2025-10-01 22:21:34 -0300

    elimina: codigo muerto

--- src/auth/utils/setUpUserBoard.ts
+++ src/auth/utils/setUpUserBoard.ts
@@ -25,14 +25,15 @@ export const setUpUserBoard = async ({
 			saveUserBoardOnSupabase(actualUserBoard)
 		} else if (data != null) {
 			const boardFromSupabase = data[0]
-			const { data: boardAccessoriesFromSupabase, error: boardAccessoriesError } = await supabase
-				.from('board_accessories')
-				.select('*')
-				.eq('id', boardFromSupabase.id)
-				.single()
-			
-			if(boardAccessoriesError) console.error(boardAccessoriesError)
-			
+			const { data: boardAccessoriesFromSupabase, error: boardAccessoriesError } =
+				await supabase
+					.from('board_accessories')
+					.select('*')
+					.eq('id', boardFromSupabase.id)
+					.single()
+
+			if (boardAccessoriesError) console.error(boardAccessoriesError)
+
 			const savedUserBoard: UserBoard = {
 				board: boardFromSupabase,
 				accessories: boardAccessoriesFromSupabase,
