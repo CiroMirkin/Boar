4ea1d7dc12b0604a1703c754bf0ed84eb8b8f011
Ciro Mirkin <ciromirkin@gmail.com>
2025-10-18 21:34:41 -0300

    elimina: archivos con funciones obsoletas debido a useTagsQuery

--- src/sharedByModules/utils/setUserBoardByDefault.ts
+++
@@ -1,7 +0,0 @@
-import { Dispatch } from '@reduxjs/toolkit'
-import { changeActualTagGroup } from '@/modules/taskList/Tags/state/tagsReducer'
-import { eisenhowerTagGroup } from '@/modules/taskList/Tags/model/defaultTags'
-
-export const setBoardByDefault = (dispatch: Dispatch): void => {
-	dispatch(changeActualTagGroup(eisenhowerTagGroup))
-}
