5305de1a7f6d2843db711168c60afe538213ef9d
Ciro Mirkin <ciromirkin@gmail.com>
2025-10-24 22:40:25 -0300

    fix: wrong type declaration

--- src/modules/columnList/useCase/addColumn.test.ts
+++ src/modules/columnList/useCase/addColumn.test.ts
@@ -1,5 +1,5 @@
 import { Column } from '../models/column'
-import { addColumnAtTheEnd } from './actions/addColumn'
+import { addColumnAtTheEnd } from './addColumn'
 import { expect } from 'vitest'
 
 describe('Crear una columna.', () => {
