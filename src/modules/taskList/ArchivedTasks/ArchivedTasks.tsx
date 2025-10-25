e4310236f9b7f4802e845517afc01816f193747f
Ciro Mirkin <ciromirkin@gmail.com>
2025-10-24 23:40:13 -0300

    implementa: componente Footer especifico para el modulo ArchivedTasks

--- src/modules/taskList/ArchivedTasks/ArchivedTasks.tsx
+++ src/modules/taskList/ArchivedTasks/ArchivedTasks.tsx
@@ -1,32 +1,12 @@
-import { Button } from '@/ui/atoms/button'
 import { useArchive } from '@/modules/taskList/ArchivedTasks/hooks/useArchive'
-import { downloadArchiveLikePDF } from '@/modules/taskList/ArchivedTasks/downloadArchiveLikePDF'
 import { useTranslation } from 'react-i18next'
-import { useTheme } from '@/sharedByModules/hooks/useTheme'
 import { ArchiveContent } from './components/ArchiveContent'
-import { toast } from 'sonner'
 import { EmptySpaceText } from '@/ui/atoms/EmptySpaceText'
-import { useArchivedTasksQuery } from './hooks/useArchivedTasksQuery'
-import { emptyArchivedTasks } from './models/archive'
+import Footer from './components/Footer'
 
 export function ArchivedTasks() {
 	const { t } = useTranslation()
-	const { column } = useTheme()
 	const boardArchive = useArchive()
-
-	const { updateArchivedTasks } = useArchivedTasksQuery()
-	const cleanTheWholeArchive = () => {
-		updateArchivedTasks(emptyArchivedTasks)
-	}
-
-	const askForConfirmationToCleanTheWholeArchive = () => {
-		toast.warning(t('archive.clean_archive_warning'), {
-			action: {
-				label: t('archive.clean_archive_btn'),
-				onClick: cleanTheWholeArchive,
-			},
-		})
-	}
 
 	return (
 		<>
@@ -36,20 +16,7 @@ export function ArchivedTasks() {
 				) : (
 					<div className='max-w-3xl flex flex-col gap-y-2'>
 						<ArchiveContent />
-						<footer className={`mb-4 p-4 rounded-lg flex flex-col gap-2 ${column}`}>
-							<Button
-								variant='outline'
-								onClick={() => downloadArchiveLikePDF({ archive: boardArchive })}
-							>
-								{t('archive.archive_to_pdf_btn')}
-							</Button>
-							<Button
-								variant='destructiveGhost'
-								onClick={askForConfirmationToCleanTheWholeArchive}
-							>
-								{t('archive.clean_archive_btn')}
-							</Button>
-						</footer>
+						<Footer />
 					</div>
 				)}
 			</div>
