5bf23d7b73d30b453e1db931a7575a358688f7ec
Ciro Mirkin <ciromirkin@gmail.com>
2025-10-24 23:48:45 -0300

    agrega: componentes especificos DeleteArchivedTaskButton y ReturnTaskToBoardButton dentro del modulo ArchivedTasks

---
+++ src/modules/taskList/ArchivedTasks/components/ReturnTaskToBoardButton.tsx
@@ -0,0 +1,41 @@
+import { useContext } from 'react'
+import { TaskContext } from '../../../../ui/organisms/BlankTask'
+import { Button } from '@/ui/atoms/button'
+import { toast } from 'sonner'
+import { useTranslation } from 'react-i18next'
+import { useListOfTasksInColumnsQuery } from '../../hooks/useListOfTasksInColumnsQuery'
+import { addTaskInTheLastColumn } from '../../useCase/addTask'
+import { sortListOfTasksInColumnsByPriority } from '../../models/sortListOfTasksInColumnsByPriority'
+import { useArchivedTasksQuery } from '../hooks/useArchivedTasksQuery'
+import { deleteThisArchivedTask } from '../useCase/deleteArchivedTask'
+
+export function ReturnTaskToBoardButton() {
+	const { t } = useTranslation()
+	const task = useContext(TaskContext)
+	const { listOfTaskInColumns, updateListOfTaskInColumns } = useListOfTasksInColumnsQuery()
+	const { archivedTasks, updateArchivedTasks } = useArchivedTasksQuery()
+
+	const returnTaskToLastColumnAction = () => {
+		const updatedListOfTaskInColumns = sortListOfTasksInColumnsByPriority(
+			addTaskInTheLastColumn({
+				taskListInEachColumn: listOfTaskInColumns || [],
+				task,
+			})
+		)
+		updateListOfTaskInColumns(updatedListOfTaskInColumns)
+
+		const updatedArchivedTasks = deleteThisArchivedTask({
+			task,
+			archive: archivedTasks,
+		})
+		updateArchivedTasks(updatedArchivedTasks)
+
+		toast.info(t('archive.return_task_to_board_toast'))
+	}
+
+	return (
+		<Button size='sm' onClick={returnTaskToLastColumnAction} variant='ghost' className='w-full'>
+			{t('archive.return_task_to_board_btn')}
+		</Button>
+	)
+}
