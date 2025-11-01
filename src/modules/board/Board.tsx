2eb1b6f9a499a78a1589ba581d1219c83fe10500
Ciro Mirkin <ciromirkin@gmail.com>
2025-11-01 19:17:33 -0300

    elimina: comentarios

--- src/modules/board/Board.tsx
+++ src/modules/board/Board.tsx
@@ -4,14 +4,8 @@ import { useDocumentTitle } from '@uidotdev/usehooks'
 import { LoadingBoard } from './components/LoadingBoard'
 
 export function Board({ children }: { children: React.ReactNode }) {
-	// Usar el nuevo hook
 	const { board, isLoading, isError } = useBoardQuery()
-
-	// useDocumentTitle ahora necesita manejar el caso en que `board` es undefined
 	useDocumentTitle(board ? `${board.name} - Boar` : 'Boar')
-
-	// El hook `useSaveBoard` ya no es necesario, la mutación se encarga de guardar.
-	// Deberás llamar a `updateBoard(nuevoEstadoDelBoard)` desde donde sea que modifiques el board.
 
 	if (isLoading) {
 		return <LoadingBoard />
