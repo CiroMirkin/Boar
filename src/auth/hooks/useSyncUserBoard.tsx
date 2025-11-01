f5bb93ee344e91c9f28c8ca4e8117b1a67ffedc6
Ciro Mirkin <ciromirkin@gmail.com>
2025-11-01 20:29:25 -0300

    cambia: flujo al iniciar sesion o crear una cuenta de usuario

--- src/auth/hooks/useSyncUserBoard.tsx
+++ src/auth/hooks/useSyncUserBoard.tsx
@@ -1,19 +1,16 @@
 import { setUpUserBoard } from '../utils/setUpUserBoard'
 import { useSession } from './useSession'
 import { useEffect } from 'react'
-import { useDispatch } from 'react-redux'
 
 export const useSyncUserBoard = () => {
-	const dispatch = useDispatch()
 	const { session } = useSession()
-
 	useEffect(() => {
 		const initialStorage = async () => {
 			if (session) {
-				await setUpUserBoard({ dispatch, session })
+				await setUpUserBoard({ session })
 				sessionStorage.setItem('isInitialLoad', 'false')
 			}
 		}
 		initialStorage()
-	}, [session, dispatch])
+	}, [session])
 }
