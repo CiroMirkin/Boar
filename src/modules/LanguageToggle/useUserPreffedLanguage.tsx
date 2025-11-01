a8d479b1b410a4b9d8a1f7a4b30b20c7e03e14a0
Ciro Mirkin <ciromirkin@gmail.com>
2025-10-31 23:00:56 -0300

    fix: eslint warning en useUserPreffedLanguage

--- src/modules/LanguageToggle/useUserPreffedLanguage.tsx
+++ src/modules/LanguageToggle/useUserPreffedLanguage.tsx
@@ -1,15 +1,15 @@
 import { useEffect } from 'react'
 import { usePreferredLanguage } from '@uidotdev/usehooks'
 import { useTranslation } from 'react-i18next'
-import i18next from 'i18next'
 
 export const useUserPreffedLanguage = () => {
 	const { i18n } = useTranslation()
-	const userPrerredLanguage = usePreferredLanguage().slice(0, 2)
+	const userPreferredLanguage = usePreferredLanguage().slice(0, 2)
+
 	useEffect(() => {
-		if (userPrerredLanguage == 'en') {
-			i18next.changeLanguage(userPrerredLanguage)
+		if (userPreferredLanguage === 'en') {
+			i18n.changeLanguage(userPreferredLanguage)
 			document.body.dir = i18n.dir()
 		}
-	}, [])
+	}, [userPreferredLanguage, i18n])
 }
