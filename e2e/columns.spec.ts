921b1b294464fb4c3ba9dbb8e48398351caddc43
Ciro Mirkin <ciromirkin@gmail.com>
2025-11-18 02:57:37 -0300

    agrega: prueba de integridad a la suit de columns.spec.ts

--- e2e/columns.spec.ts
+++ e2e/columns.spec.ts
@@ -61,4 +61,48 @@ test.describe('Creacion y eliminacion de columnas', () => {
 			await expect(page.getByRole('heading', { name: columnName })).toBeVisible()
 		})
 	})
+
+	test('Al crear y eliminar tareas se mantiene la consistencia entre las tareas creadas y las columnas', async ({
+		page,
+	}) => {
+		await page.goto('/settings')
+		const columnName = `Columna Prueba ${Date.now()}`
+		const taskName = `Tarea Prueba ${Date.now()}`
+
+		await test.step('Creo una nueva columna', async () => {
+			await page.getByPlaceholder('Nombre de la columna').fill(columnName)
+			await page.getByTestId('BotonParaCrearUnaColumna').click()
+			await expect(page.locator(`input[name="${columnName}"]`)).toBeVisible()
+		})
+
+		await test.step('Creo una tarea', async () => {
+			await page.goto('/')
+			await page.fill('#add_new_task_btn', taskName)
+			await page.click('#plus_btn')
+			await expect(page.getByText(taskName)).toBeVisible()
+			await page.getByText(taskName).click()
+		})
+
+		await test.step('Muevo la tarea hasta la tercer columna', async () => {
+			const tareaEnPendientes = page.locator('[aria-label="Pendientes"]').getByText(taskName)
+			const columnaProcesando = page.locator('[aria-label="Terminado"]')
+			await tareaEnPendientes.dragTo(columnaProcesando)
+		})
+
+		await test.step('Elimino la segunda columna', async () => {
+			await page.goto('/settings')
+			await page.locator('li').nth(1).getByTestId('BotonParaEliminarUnaColumna').click()
+			await page.getByRole('button', { name: 'Eliminar', exact: true }).click()
+			await expect(page.locator('li').nth(1)).not.toHaveText('Procesando')
+		})
+
+		await test.step('Puedo eliminar las tareas de la segunda columna', async () => {
+			await page.goto('/')
+			await expect(page.getByText(taskName)).toBeVisible()
+			await page.getByText(taskName).click()
+			await page.getByTestId('BotonEliminarTarea').click()
+			await page.getByText('Eliminar', { exact: true }).click()
+			await expect(page.getByText(taskName)).not.toBeVisible()
+		})
+	})
 })
