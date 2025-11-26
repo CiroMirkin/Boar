fe2a0784f923094de6ab43cfbe14b8ed68c91641
Ciro Mirkin <107360995+CiroMirkin@users.noreply.github.com>
2025-11-26 12:56:14 +0000

    agrega: componente UsageRecord

---
+++ src/modules/UsageHistory/components/UsageRecord.tsx
@@ -0,0 +1,36 @@
+import { DailyUsage } from '../model/usageHistory'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/ui/molecules/card'
+import { formatDate } from '@/sharedByModules/utils/formatDate'
+import Period from './Period'
+import { useTheme } from '@/sharedByModules/hooks/useTheme'
+import { parseDuration } from '../utils/parseDuration'
+
+interface UsageRecordProps {
+	usageRecord: DailyUsage
+}
+
+function UsageRecord({ usageRecord }: UsageRecordProps) {
+	const { column } = useTheme()
+	const date = formatDate(new Date(usageRecord.date))
+	let totalDuration = 0
+	const periods = [...usageRecord.periods].reverse().map((period) => {
+		totalDuration += period.duration
+		return <Period key={period.startTimestamp} period={period} />
+	})
+
+	return (
+		<Card className={'pt-4 rounded-md shadow-sm ' + column}>
+			<CardHeader className='p-0'>
+				<CardTitle className='text-2xl'>{date}</CardTitle>
+				<CardDescription className='text-base !m-0' title='Total de tiempo'>
+					{parseDuration(totalDuration)}
+				</CardDescription>
+			</CardHeader>
+			<CardContent className='mt-4'>
+				<ul className='flex flex-col gap-2'>{periods}</ul>
+			</CardContent>
+		</Card>
+	)
+}
+
+export default UsageRecord
