5eb3478f321680a04bb634dfa64d5231c44229f8
Ciro Mirkin <ciromirkin@gmail.com>
2025-11-29 22:19:43 -0300

    elimina: modulo muerto ColumnList

--- src/modules/columnList/hooks/useCheckIfThisColumnIsTheLast.tsx
+++
@@ -1,7 +0,0 @@
-import { Column, isThisTheLastColumn } from '../models/column'
-import { useColumnList } from './useColumnList'
-
-export function useCheckIfThisColumnIsTheLast(column: Column): boolean {
-	const columnList = useColumnList()
-	return isThisTheLastColumn(column, columnList)
-}
--- src/modules/columnList/repository/columnListRepository.ts
+++
@@ -1,6 +0,0 @@
-import { ColumnList } from '../models/columnList'
-
-export interface ColumnListRepository {
-	save(columnList: ColumnList): void | Promise<void>
-	getAll(): Promise<ColumnList>
-}
