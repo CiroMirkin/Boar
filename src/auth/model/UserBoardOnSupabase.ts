ca7405b09935b6abacd9e401f575ea191157a177
Ciro Mirkin <ciromirkin@gmail.com>
2025-11-29 22:19:04 -0300

    implementa: sub modulo Columns dentro del modulo Auth

--- src/auth/model/UserBoardOnSupabase.ts
+++ src/auth/model/UserBoardOnSupabase.ts
@@ -1,10 +1,8 @@
-import { ColumnList } from '@/modules/columnList/models/columnList'
-import { TaskListInEachColumn } from '@/modules/taskList/models/taskList'
+import { TaskBoard } from '@/modules/taskList/models/taskBoard'
 
 export interface UserBoardOnSupabase {
 	id: string
 	user_id: string | undefined
 	name: string
-	column_list: ColumnList
-	task_list_in_each_column: TaskListInEachColumn
+	task_list_in_each_column: TaskBoard
 }
