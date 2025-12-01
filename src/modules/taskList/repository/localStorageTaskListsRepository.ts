cefa56910a5065a5bc98d82ed224e0d2b25f40bb
Ciro Mirkin <ciromirkin@gmail.com>
2025-12-01 01:01:12 -0300

    agrega: soporte y migracion del formato de datos TaskList a TaskBoard

--- src/modules/taskList/repository/localStorageTaskListsRepository.ts
+++ src/modules/taskList/repository/localStorageTaskListsRepository.ts
@@ -1,4 +1,5 @@
 import { emptyTaskBoard, TaskBoard } from '../models/taskBoard'
+import { isThisArrayOfTypeTaskListInEachColumn, TaskList } from '../models/taskList'
 import { TaskListInEachColumnRepository } from './taskListInEachColumnRepository'
 
 export default class LocalStorageTaskListInEachColumnRepository
@@ -11,11 +12,31 @@ export default class LocalStorageTaskListInEachColumnRepository
 	async save(taskBoard: TaskBoard): Promise<void> {
 		localStorage.setItem(this.key, JSON.stringify(taskBoard))
 	}
-	async getAll() {
+	async getAll(): Promise<TaskBoard> {
 		const stored = localStorage.getItem(this.key)
 		if (stored) {
-			return JSON.parse(stored)
+			const parsed = JSON.parse(stored)
+			
+			// Detecta y migra el formato legacy (array de TaskList) al nuevo formato TaskBoard
+			if (isThisArrayOfTypeTaskListInEachColumn(parsed)) {
+				const storedColumns = localStorage.getItem('columnList')
+				const defaultColumns = JSON.parse(
+					'[{"id":"c1","name":"default_columns.c1","position":"1"},{"id":"c2","name":"default_columns.c2","position":"2"},{"id":"c3","name":"default_columns.c3","position":"3"}]'
+				)
+				const localColumns = storedColumns ? JSON.parse(storedColumns) : defaultColumns
+				const taskBoard = parsed.map((taskList: TaskList, i: number) => ({
+					id: localColumns[i].id,
+					tasks: taskList,
+					status: localColumns[i].name,
+				}))
+
+				await this.save(taskBoard)
+				return taskBoard
+			}
+
+			return parsed
 		}
+
 		await this.save(emptyTaskBoard)
 		return emptyTaskBoard
 	}
