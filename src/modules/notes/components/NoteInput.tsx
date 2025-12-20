494fa964868a26e9902700268941a846144cb079
Ciro Mirkin <ciromirkin@gmail.com>
2025-12-19 23:54:57 -0300

    cambia: interfaz del componente MinimalTiptapEditor mejorando su funcionamiento

--- src/modules/notes/components/NoteInput.tsx
+++ src/modules/notes/components/NoteInput.tsx
@@ -1,35 +1,45 @@
 import { MinimalTiptapEditor } from '@/ui/organisms/MinimalTiptapEditor'
-import { maxLengthOfNotes, Notes as NotesModel } from '../model/notes'
+import { maxLengthOfNotes } from '../model/notes'
 import { toast } from 'sonner'
 import { useTranslation } from 'react-i18next'
 import { useNotesQuery } from '../hooks/useNotesQuery'
 import { Spinner } from '@/ui/atoms/spinner'
+import { useCallback, useEffect, useState } from 'react'
 
 export function NoteInput() {
 	const { t } = useTranslation()
 	const { notes, updateNotes, isLoading } = useNotesQuery()
+	const [notesValue, setNotesValue] = useState(notes)
 
-	const onChange = (newText: NotesModel) => {
-		if (newText.trim().length <= maxLengthOfNotes) {
-			updateNotes(newText)
+	const saveNotes = useCallback(() => {
+		if (notesValue.trim().length <= maxLengthOfNotes) {
+			updateNotes(notesValue)
+			toast.success(t('notes.successful_toast'))
 			return
 		}
 
 		toast.error(t('notes.warning_length_toast'))
-	}
+	}, [notesValue, t, updateNotes])
+
+	useEffect(() => {
+		if (notesValue !== notes) {
+			const timeoutId = setTimeout(() => {
+				saveNotes()
+			}, 500)
+			return () => clearTimeout(timeoutId)
+		}
+	}, [notesValue, notes, saveNotes])
 
 	if (isLoading) return <Spinner size={30} />
 
 	return (
 		<>
 			<MinimalTiptapEditor
-				value={notes || ''}
-				onChange={onChange}
+				value={notes}
+				onChange={setNotesValue}
 				rows={5}
 				maxRows={18}
-				saveTextCallback={() => {
-					toast.success(t('notes.successful_toast'))
-				}}
+				onSave={saveNotes}
 			/>
 		</>
 	)
