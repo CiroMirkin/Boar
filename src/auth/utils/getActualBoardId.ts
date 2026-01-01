ef735c08204791794d12c366690232c829f099d3
Ciro Mirkin <ciromirkin@gmail.com>
2025-12-31 22:09:26 -0300

    refactor: getActualBoardId para mejores resultado e interfaz clara

--- src/auth/utils/getActualBoardId.ts
+++ src/auth/utils/getActualBoardId.ts
@@ -1,16 +1,14 @@
-import { defaultBoard } from '@/modules/board/models/board'
-
 type BoardId = string
 
 /**
- * Optiene el board_id directamente de la URL.
- * @description Esta funcion cuenta con que la URL sea:
- * > board_domain/board/{board_id}
- * @returns { string } Si no encuentra un id en la URL retorna el id del tablero por defecto.
+ * Obtiene el board_id directamente de la URL.
+ * @description Esta funcion asume que la URL es:
+ * > board_domain/{something}/{board_id}
+ * @returns { string } Si no encuentra un id en la URL retorna un string vacio.
  * */
 export const getActualBoardId = (): BoardId => {
 	const { pathname } = window.location
 
 	const board_id = pathname.split('/')[2]
-	return board_id || defaultBoard.id
+	return board_id ?? ''
 }
