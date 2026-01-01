e27773d973813ae706cb46a051321599e3afe7b0
Ciro Mirkin <ciromirkin@gmail.com>
2025-12-31 22:27:02 -0300

    refactor: useTheme para proveer valores por defecto

--- src/common/hooks/useTheme.tsx
+++ src/common/hooks/useTheme.tsx
@@ -1,4 +1,12 @@
 import { useContext } from 'react'
 import { ThemeContext } from '../../modules/Theme/ThemeContext'
+import { Theme } from '@/modules/Theme/themesList'
 
-export const useTheme = () => useContext(ThemeContext).theme
+export const useTheme = (): Theme => {
+	const theme = useContext(ThemeContext).theme
+	return {
+		taskText: theme.taskText || 'text-black',
+		columnText: theme.columnText || 'text-black',
+		...theme,
+	}
+}
